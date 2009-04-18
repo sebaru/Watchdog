b96150ac34b57104ba8ede179da402487904dab7
seb <seb@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2009-04-18 15:51:54 +0000

    J'ai trouvé
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@142 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Watchdogd/Modbus/Modbus.c
+++ Watchdogd/Modbus/Modbus.c
@@ -93,18 +93,11 @@
      }
 
     while ( Recuperer_ligne_SQL (Config.log, db) )
-     { struct MODULE_MODBUS *module;
-printf("1 %s\n", db->row[0]);
-printf("1bis %d\n", atoi(db->row[0]));
-       module->id       = id;
+     { module->id       = id;
        module->actif    = atoi (db->row[0]);
-printf("2\n");
        g_snprintf( module->ip, sizeof(module->ip), "%s", db->row[1] );
-printf("3\n");
        module->bit      = atoi (db->row[2]);         /* Bit interne B d'etat communication avec le module */
-printf("4\n");
        module->watchdog = atoi (db->row[3]);
-printf("5\n");
                                                                         /* Ajout dans la liste de travail */
        Info_n( Config.log, DEBUG_MODBUS, "Charger_modules_MODBUS:  id       = ", module->id       );
        Info_n( Config.log, DEBUG_MODBUS, "                      -  actif    = ", module->actif    );
