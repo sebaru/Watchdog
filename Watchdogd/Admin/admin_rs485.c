ff557c146e523a6a2f4fe2d6d01d92db5ca34682
seb <seb@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2009-04-25 15:34:53 +0000

    Modification affichage rs485
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@255 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Watchdogd/Admin/admin_rs485.c
+++ Watchdogd/Admin/admin_rs485.c
@@ -57,7 +57,7 @@
        module = (struct MODULE_RS485 *)liste_modules->data;
 
        g_snprintf( chaine, sizeof(chaine),
-                   "\n RS485[%02d] -> actif=%d,ea=%03d-%03d,e=%03d-%03d,ec=%03d-%03d,s=%03d-%03d,"
+                   " RS485[%02d] -> actif=%d,ea=%03d-%03d,e=%03d-%03d,ec=%03d-%03d,s=%03d-%03d,"
                    "sa=%03d-%03d,req=%d,ret=%d,ana=%d\n",
                    module->id, module->actif, module->ea_min, module->ea_max,
                    module->e_min, module->e_max, module->ec_min, module->ec_max,
