ec903fbef07912fd1314df1d6cf4089e68f828b1
seb <seb@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2010-05-23 19:54:40 +0000

    Modification licence Commun + ajout doxygen.conf
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@597 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Commun/Unite.c
+++ Commun/Unite.c
@@ -1,31 +1,30 @@
 /**********************************************************************************************************/
-/* Commun/unite.c        Gestion des unites entreeANAs de Watchdog v2.0                                   */
+/* Commun/Unite.c        Gestion des unites entreeANAs de Watchdog v2.0                                   */
 /* Projet WatchDog version 2.0       Gestion d'habitat                     dim. 15 nov. 2009 13:43:06 CET */
 /* Auteur: LEFEVRE Sebastien                                                                              */
 /**********************************************************************************************************/
 /*
  * Unite.c
- * This file is part of <program name>
+ * This file is part of Watchdog
  *
- * Copyright (C) 2009 - 
+ * Copyright (C) 2010 - Sebastien LEFEVRE
  *
- * <program name> is free software; you can redistribute it and/or modify
+ * Watchdog is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * <program name> is distributed in the hope that it will be useful,
+ * Watchdog is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with <program name>; if not, write to the Free Software
+ * along with Watchdog; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, 
  * Boston, MA  02110-1301  USA
  */
- 
- 
+
  #include <glib.h>
  #include <string.h>
  #include "Cst_entreeana.h"
@@ -61,17 +60,6 @@
  gchar *Unite_vers_string ( guint type )
   { if (type<NBR_TYPE_UNITE) return( UNITE_EA[type] );
                         else return ( "Unknown" );
-  }
-/**********************************************************************************************************/
-/* String_vers_unite: renvoie l'unite en tant que int                                                     */
-/* Entrée: rien                                                                                           */
-/* Sortie: Niet                                                                                           */
-/**********************************************************************************************************/
- gint String_vers_unite ( guchar *unite )
-  { gint i;
-    for (i=0; i<NBR_TYPE_UNITE; i++)
-     { if ( !strcmp( UNITE_EA[i], unite ) ) return(i); }
-    return(0);
   }
 /**********************************************************************************************************/
 /* Unite_vers_string: renvoie l'unite sous forme de chaine de caractere                                   */
