20bf97a2a93381127ad33577dc69c3e4cb22b9a1
seb <seb@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2010-05-23 20:09:45 +0000

    Modif licence GPL dans Watchdogd. Reste les sous-repertoires
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@600 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Watchdogd/Watchdogd.c
+++ Watchdogd/Watchdogd.c
@@ -7,7 +7,7 @@
  * Watchdogd.c
  * This file is part of Watchdog
  *
- * Copyright (C) 2008 - sebastien
+ * Copyright (C) 2010 - Sebastien LEFEVRE
  *
  * Watchdog is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, 
  * Boston, MA  02110-1301  USA
  */
- 
+
  #include <glib.h>
  #include <openssl/ssl.h>
  #include <openssl/err.h>
--- Watchdogd/process.c
+++ Watchdogd/process.c
@@ -7,7 +7,7 @@
  * process.c
  * This file is part of Watchdog
  *
- * Copyright (C) 2009 - sebastien
+ * Copyright (C) 2010 - Sebastien LEFEVRE
  *
  * Watchdog is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, 
  * Boston, MA  02110-1301  USA
  */
- 
+
  #include <glib.h>
  #include <bonobo/bonobo-i18n.h>
  #include <sys/wait.h>
