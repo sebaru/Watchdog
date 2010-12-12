ae5e087e6c6f948c096a0f591707a974a41133c7
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2010-12-12 19:11:22 +0000

    Modification de la lecture config cliente. Utilisation de GLib au lieu de LEX/YACC
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@862 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Client/Config_cli/Config_cli.c
+++ Client/Config_cli/Config_cli.c
@@ -42,26 +42,110 @@
 /* Entrée: La structure à remplir, le nom du fichier originel                                             */
 /**********************************************************************************************************/
  void Lire_config_cli ( struct CONFIG_CLI *config_cli, char *fichier_config_cli )
-  { char *fichier;
-    FILE *rc;
+  { gchar *chaine, *fichier;
+    gint debug;
+    GKeyFile *gkf;
 
     if (!config_cli) return;
 
-    config_cli->port                  = DEFAUT_PORT;
-    config_cli->ssl_crypt             = DEFAUT_SSL_CRYPT;
-    config_cli->taille_bloc_reseau    = DEFAUT_TAILLE_BLOC_RESEAU;
-    config_cli->debug_level           = DEFAUT_DEBUG_LEVEL;
-    g_snprintf( config_cli->serveur, sizeof(config_cli->serveur), "%s", DEFAUT_SERVEUR );
-    g_snprintf( config_cli->user,    sizeof(config_cli->user),    "%s", DEFAUT_USER );
-
     if (!fichier_config_cli) fichier = DEFAUT_FICHIER_CONFIG_CLI;
                         else fichier = fichier_config_cli;
-    rc = fopen( fichier, "r" );
-    if (rc)
-     { Interpreter(rc, config_cli);
-       fclose(rc);
+
+    gkf = g_key_file_new();
+
+    if (g_key_file_load_from_file(gkf, fichier, G_KEY_FILE_NONE, NULL))
+     {
+
+/********************************************* Partie SERVEUR *********************************************/
+       config_cli->taille_bloc_reseau = g_key_file_get_integer ( gkf, "SERVER", "taille_bloc_reseau", NULL );
+       if (!config_cli->taille_bloc_reseau) config_cli->taille_bloc_reseau = DEFAUT_TAILLE_BLOC_RESEAU;
+
+       config_cli->ssl_crypt          = g_key_file_get_integer ( gkf, "SERVER", "ssl_crypt", NULL );
+       if (!config_cli->ssl_crypt) config_cli->ssl_crypt = DEFAUT_SSL_CRYPT;
+
+       chaine = g_key_file_get_string ( gkf, "SERVER", "host", NULL );
+       if (chaine)
+        { g_snprintf( config_cli->serveur, sizeof(config_cli->serveur), "%s", chaine ); g_free(chaine); }
+       else
+        { g_snprintf( config_cli->serveur, sizeof(config_cli->serveur), "%s", DEFAUT_SERVEUR  ); }
+
+       chaine = g_key_file_get_string ( gkf, "SERVER", "user", NULL );
+       if (chaine)
+        { g_snprintf( config_cli->user, sizeof(config_cli->user), "%s", chaine ); g_free(chaine); }
+       else
+        { g_snprintf( config_cli->user, sizeof(config_cli->user), "%s", DEFAUT_USER  ); }
+
+       config_cli->port              = g_key_file_get_integer ( gkf, "SERVER", "port", NULL );
+       if (!config_cli->port) config_cli->port = DEFAUT_PORT;
+
+/********************************************* Partie DEBUG ***********************************************/
+       config_cli->debug_level = 0;
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_SIGNAUX", NULL );
+       if (debug) config_cli->debug_level += DEBUG_SIGNAUX;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_DB", NULL );
+       if (debug) config_cli->debug_level += DEBUG_DB;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_CONFIG", NULL );
+       if (debug) config_cli->debug_level += DEBUG_CONFIG;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_USER", NULL );
+       if (debug) config_cli->debug_level += DEBUG_USER;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_CRYPTO", NULL );
+       if (debug) config_cli->debug_level += DEBUG_CRYPTO;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_INFO", NULL );
+       if (debug) config_cli->debug_level += DEBUG_INFO;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_SERVEUR", NULL );
+       if (debug) config_cli->debug_level += DEBUG_SERVEUR;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_CDG", NULL );
+       if (debug) config_cli->debug_level += DEBUG_CDG;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_NETWORK", NULL );
+       if (debug) config_cli->debug_level += DEBUG_NETWORK;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_ARCHIVE", NULL );
+       if (debug) config_cli->debug_level += DEBUG_ARCHIVE;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_CONNEXION", NULL );
+       if (debug) config_cli->debug_level += DEBUG_CONNEXION;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_DLS", NULL );
+       if (debug) config_cli->debug_level += DEBUG_DLS;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_MODBUS", NULL );
+       if (debug) config_cli->debug_level += DEBUG_MODBUS;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_ADMIN", NULL );
+       if (debug) config_cli->debug_level += DEBUG_ADMIN;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_RS485", NULL );
+       if (debug) config_cli->debug_level += DEBUG_RS485;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_ONDULEUR", NULL );
+       if (debug) config_cli->debug_level += DEBUG_ONDULEUR;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_SMS", NULL );
+       if (debug) config_cli->debug_level += DEBUG_SMS;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_AUDIO", NULL );
+       if (debug) config_cli->debug_level += DEBUG_AUDIO;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_CAMERA", NULL );
+       if (debug) config_cli->debug_level += DEBUG_CAMERA;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_COURBE", NULL );
+       if (debug) config_cli->debug_level += DEBUG_COURBE;
+
+       debug = g_key_file_get_boolean ( gkf, "DEBUG", "debug_ALL", NULL );
+       if (debug) config_cli->debug_level = ~0;
+
+       if (!config_cli->debug_level) config_cli->debug_level = DEFAUT_DEBUG_LEVEL;          /* Par défaut */
      }
-    else printf( "file %s unknown\n", fichier );
+    g_key_file_free(gkf);
   }
 /**********************************************************************************************************/
 /* Print_config_cli: Affichage (enfin log) la config actuelle en parametre                                */
