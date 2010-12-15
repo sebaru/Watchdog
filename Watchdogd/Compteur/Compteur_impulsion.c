59b412b534330d2d2c3d950197f2362f627038ca
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2010-12-15 11:59:26 +0000

    Correction requete SQL chargement CI
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@875 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Watchdogd/Compteur/Compteur_impulsion.c
+++ Watchdogd/Compteur/Compteur_impulsion.c
@@ -91,7 +91,7 @@
                 "SELECT id_mnemo,val,unite,num"
                 " FROM %s,%s WHERE %s.id=%s.id_mnemo ORDER BY %s.num",
                 NOM_TABLE_CPT_IMP, NOM_TABLE_MNEMO, /* From */
-                NOM_TABLE_MNEMO, NOM_TABLE_CPT_IMP, NOM_TABLE_MNEMO /* WHERE */
+                NOM_TABLE_MNEMO, NOM_TABLE_CPT_IMP, /* WHERE */
                 NOM_TABLE_MNEMO /* Order by */
               );
 
