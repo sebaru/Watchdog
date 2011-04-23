6dc1a501768c9424990327f8ccc06bb2eab2a623
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2011-04-23 13:02:02 +0000

    Ajout d'affichage last_arch pour getea dans admin_runnning
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@1086 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Watchdogd/Admin/admin_running.c
+++ Watchdogd/Admin/admin_running.c
@@ -203,10 +203,12 @@
     if ( ! strcmp ( commande, "getea" ) )
      { int num;
        sscanf ( ligne, "%s %d", commande, &num );                    /* Découpage de la ligne de commande */
+       g_snprintf( chaine, sizeof(chaine), " Partage->Top : %d\n", Partage->top );
+       Write_admin ( client->connexion, chaine );
        g_snprintf( chaine, sizeof(chaine),
-                   " EA%03d = %8.2f, val_int=%d, inrange=%d, type=%d, min=%8.2f, max=%8.2f\n",
+                   " EA%03d = %8.2f, val_int=%d, inrange=%d, type=%d, last_arch=%d, min=%8.2f, max=%8.2f\n",
                    num, EA_ech(num), Partage->ea[num].val_int, EA_inrange(num),
-                   Partage->ea[num].cmd_type_eana.type,
+                   Partage->ea[num].cmd_type_eana.type, (int)Partage->ea[num].last_arch, 
                    Partage->ea[num].cmd_type_eana.min, Partage->ea[num].cmd_type_eana.max 
                  );
        Write_admin ( client->connexion, chaine );
