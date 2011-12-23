79ffa1a5f1f6704badc6dce64c3facacf8c249f7
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2011-12-23 17:27:28 +0000

    Retrait de la colonne SSGROUPE dans la liste synoptique sur le client
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@1256 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Client/liste_synoptique.c
+++ Client/liste_synoptique.c
@@ -305,20 +305,13 @@ printf("on veut editer(atelier) le synoptique %d, %s\n", rezo_synoptique.id, rez
 
     renderer = gtk_cell_renderer_text_new();                                    /* Colonne du commentaire */
     colonne = gtk_tree_view_column_new_with_attributes ( _("Page"), renderer,
-                                                         "text", COLONNE_SSGROUPE,
-                                                         NULL);
-    gtk_tree_view_column_set_sort_column_id (colonne, COLONNE_SSGROUPE);
-    gtk_tree_view_append_column ( GTK_TREE_VIEW (Liste_synoptique), colonne );
-
-    renderer = gtk_cell_renderer_text_new();                                    /* Colonne du commentaire */
-    colonne = gtk_tree_view_column_new_with_attributes ( _("Titre"), renderer,
                                                          "text", COLONNE_TITRE,
                                                          NULL);
     gtk_tree_view_column_set_sort_column_id (colonne, COLONNE_TITRE);
     gtk_tree_view_append_column ( GTK_TREE_VIEW (Liste_synoptique), colonne );
 
     renderer = gtk_cell_renderer_text_new();                          /* Colonne du libelle de synoptique */
-    colonne = gtk_tree_view_column_new_with_attributes ( _("Libelle"), renderer,
+    colonne = gtk_tree_view_column_new_with_attributes ( _("Name"), renderer,
                                                          "text", COLONNE_LIBELLE,
                                                          NULL);
     gtk_tree_view_column_set_sort_column_id(colonne, COLONNE_LIBELLE);                /* On peut la trier */
