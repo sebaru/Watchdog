9f218486dbe4807aa154ee16689490fa6be3d4cb
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2012-03-24 12:42:32 +0000

    Correction setlocale pour chargement des CI depuis la base de données
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@1310 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Watchdogd/Compteur/Compteur_impulsion.c
+++ Watchdogd/Compteur/Compteur_impulsion.c
@@ -120,11 +120,13 @@
     cpt_imp = (struct CMD_TYPE_OPTION_COMPTEUR_IMP *)g_malloc0( sizeof(struct CMD_TYPE_OPTION_COMPTEUR_IMP) );
     if (!cpt_imp) Info( log, DEBUG_INFO, "Rechercher_cpt_impDB: Erreur allocation mémoire" );
     else
-     { cpt_imp->id_mnemo = atoi(db->row[0]);
+     { setlocale( LC_NUMERIC, "C" );                 /* Pour le formattage correct des , . dans les float */
+       cpt_imp->id_mnemo = atoi(db->row[0]);
        cpt_imp->valeur   = atof(db->row[1]);
        cpt_imp->num      = atoi(db->row[2]);
        cpt_imp->type     = atoi(db->row[3]);
        cpt_imp->multi    = atof(db->row[4]);
+       setlocale( LC_NUMERIC, "" );                  /* Pour le formattage correct des , . dans les float */
        memcpy( &cpt_imp->unite, db->row[5], sizeof(cpt_imp->unite) );
      }
     return(cpt_imp);
