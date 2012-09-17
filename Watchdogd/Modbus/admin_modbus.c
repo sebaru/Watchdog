5d723d268df9b0c9ab58dfe609aed3a6b54af0c4
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2012-09-17 16:27:24 +0000

    Amelioration affichage resultat commande modbus list
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@1686 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Watchdogd/Modbus/admin_modbus.c
+++ Watchdogd/Modbus/admin_modbus.c
@@ -63,7 +63,7 @@
        g_snprintf( chaine, sizeof(chaine),
                    " MODBUS[%02d] -> bit=%04d, enable=%d, started=%d, mode=%02d, watchdog=%03d, IP=%s,\n"
                    "               min_e_tor=%03d, min_e_ana=%03d, min_s_tor=%03d, min_s_ana=%03d\n"
-                   "               trans.=%06d, deco.=%02d, last_reponse=%03ds ago, retente=in %03d, date_next_eana=in %03d\n",
+                   "               trans.=%06d, deco.=%02d, last_reponse=%03ds ago, retente=in %03ds, date_next_eana=in %03ds\n",
                    module->modbus.id, module->modbus.bit, module->modbus.enable,
                    module->started, module->mode, module->modbus.watchdog, module->modbus.ip,
                    module->modbus.min_e_tor, module->modbus.min_e_ana,
