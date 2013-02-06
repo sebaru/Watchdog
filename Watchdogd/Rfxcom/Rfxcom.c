0027195c38f072a9c00ca644bcfc63e83660aff3
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2013-02-06 20:02:37 +0000

    Ciblage des protocoles RFXCOM sur Unencoded,ARC,LaCrosse et OregonScientific - Test
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@1856 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Watchdogd/Rfxcom/Rfxcom.c
+++ Watchdogd/Rfxcom/Rfxcom.c
@@ -309,7 +309,7 @@
  static int Init_rfxcom ( void )
   { gchar trame_reset[] = { 0x0D, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00, 00 };
     gchar trame_get_status[] = { 0x0D, 00, 00, 01, 02, 00, 00, 00, 00, 00, 00, 00, 00, 00 };
-    gchar trame_set_proto[] = { 0x0D, 00, 00, 02, 03, 0x53, 00, 0x80, 0x2F, 0x00, 00, 00, 00, 00 };
+    gchar trame_set_proto[] = { 0x0D, 00, 00, 02, 03, 0x53, 00, 0x00, 0x00, 0x27, 00, 00, 00, 00 };
            /* Attention dans la doc RFXCOM il faut remplir msg5 alors qu'en reel, il faut remplir msg4 !! */
     struct termios oldtio;
     int fd;
