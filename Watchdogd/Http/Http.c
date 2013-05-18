b460fda457cfd13275d3219c671c7927b3c5d278
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2013-05-18 14:53:46 +0000

    Correction indentation
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@2067 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Watchdogd/Http/Http.c
+++ Watchdogd/Http/Http.c
@@ -58,7 +58,8 @@
     if ( ! g_key_file_load_from_file(gkf, Config.config_file, G_KEY_FILE_NONE, &error) )
      { Info_new( Config.log, TRUE, LOG_CRIT,
                  "Http_Lire_config : unable to load config file %s: %s", Config.config_file, error->message );
-       g_error_free(error);       return;
+       g_error_free(error);
+       return;
      }
                                                                                /* Positionnement du debug */
     Cfg_http.lib->Thread_debug = g_key_file_get_boolean ( gkf, "HTTP", "debug", NULL ); 
