166732e2e5b4e385ff9ea385d0549051d7e110af
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2013-11-09 11:27:50 +0000

    Ajout de debug dans Info_DB_SQL
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@2332 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Watchdogd/db.c
+++ Watchdogd/db.c
@@ -107,6 +107,7 @@
     Partage->com_db.Liste = g_slist_prepend ( Partage->com_db.Liste, db );
     taille = g_slist_length ( Partage->com_db.Liste );
     pthread_mutex_unlock ( &Partage->com_db.synchro );
+sleep(5);
     Info_new( Config.log, Config.log_db, LOG_DEBUG,
               "Init_DB_SQL: Database Connection OK with %s@%s (id=%05d). Nbr_requete_en_cours=%d",
                Config.db_username, Config.db_database, db->id, taille );
