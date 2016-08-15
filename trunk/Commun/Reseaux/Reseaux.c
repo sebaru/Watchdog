d89a153db9c3a2c4c116e05009a3cc086ec34230
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2016-08-15 08:13:41 +0000

    Suppression du try_again sur lecture SSL_read
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@2837 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Commun/Reseaux/Reseaux.c
+++ trunk/Commun/Reseaux/Reseaux.c
@@ -140,10 +140,7 @@ one_again:
 /******************************************************************************************************************************/
  static gint Recevoir_reseau_with_ssl ( struct CONNEXION *connexion, gchar *buffer, gint taille_buffer )
   { gint retour, ssl_err;
-    gint cpt;
 
-    cpt=0;
-try_again:
     retour = SSL_read( connexion->ssl, buffer, taille_buffer );                                            /* Envoi du buffer */
     if (retour > 0)
      { Info_new( connexion->log, FALSE, LOG_DEBUG,
@@ -152,17 +149,9 @@ try_again:
      }
 
     switch ( ssl_err = SSL_get_error( connexion->ssl, retour ) )
-     { case SSL_ERROR_NONE: return(0);
+     { case SSL_ERROR_NONE: 
        case SSL_ERROR_WANT_READ:
-       case SSL_ERROR_WANT_WRITE:
-        { /*cpt ++;
-          if ( !(cpt % 200))
-           { Info_new( connexion->log, FALSE, LOG_WARNING,
-                   "Recevoir_reseau_with_ssl: Socket %d, SSL error %d (retour=%d) -> %s - Retrying !",
-                    connexion->socket,ssl_err, retour, ERR_error_string( ssl_err, NULL ) );
-           }*/
-          goto try_again;
-        }
+       case SSL_ERROR_WANT_WRITE: return(0);
      }
     Info_new( connexion->log, FALSE, LOG_ERR,
              "Recevoir_reseau_with_ssl: Socket %d, SSL error %d (retour=%d) -> %s",
