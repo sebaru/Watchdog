cf8c8e203c528990fa00badd5a1e8ae6d84f0861
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2017-01-01 10:57:55 +0000

    Creation de la branche Revue_archivage
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@2953 9f22af27-4b39-4cfe-9f85-4bed9fef4693

---
+++ branches/Revue_archivage/Watchdogd/Http/Http.h
@@ -0,0 +1,109 @@
+/******************************************************************************************************************************/
+/* Watchdogd/HttpMobile/HttpMobile.h        Déclaration structure internes des WebServices                                    */
+/* Projet WatchDog version 2.0       Gestion d'habitat                                       mer. 24 avril 2013 18:48:19 CEST */
+/* Auteur: LEFEVRE Sebastien                                                                                                  */
+/******************************************************************************************************************************/
+/*
+ * httpmobile.h
+ * This file is part of Watchdog
+ *
+ * Copyright (C) 2010 - Sebastien Lefevre
+ *
+ * Watchdog is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Watchdog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Watchdog; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
+ */
+ 
+#ifndef _HTTP_H_
+ #define _HTTP_H_
+ #include <libwebsockets.h>
+ #include <libxml/xmlwriter.h>
+ 
+ #define NOM_THREAD                    "http"
+ #define HTTP_DEFAUT_FILE_CA           "http_cacert.pem"
+ #define HTTP_DEFAUT_FILE_CERT         "http_serveursigne.pem"
+ #define HTTP_DEFAUT_FILE_KEY          "http_serveurkey.pem"
+ #define HTTP_DEFAUT_SSL_CIPHER        "HIGH:NORMAL"
+ #define HTTP_DEFAUT_MAX_CONNEXION     16
+ #define HTTP_DEFAUT_TCP_PORT          5560
+
+ enum WS_PROTO
+  {	/* always first */
+    WS_PROTO_HTTP = 0,
+    WS_PROTO_STATUS,
+    /* always last */
+    WS_NBR_PROTO
+  };
+
+ struct HTTP_CONFIG
+  { struct LIBRAIRIE *lib;
+    gboolean Thread_reload;                                              /* TRUE si le thread doit recharger sa configuration */
+
+    struct lws_context_creation_info ws_info;                                             /* Paramétrage du context WebSocket */
+    struct lws_context *ws_context;                                                                      /* Context WebSocket */
+    
+    gint nbr_max_connexion;                                           /* Nombre maximum de connexion autorisées simultanément */
+    gint tcp_port;                                           /* Port d'écoute TCP (HTTP ou HTTPS, selon le paramètre suivant) */
+    gboolean ssl_enable;                                                                             /* TRUE si connexion SSL */
+    gchar ssl_cipher_list[128];
+    gchar ssl_cert_filepath[80];
+    gchar ssl_private_key_filepath[80];
+    gchar ssl_ca_filepath[80];
+    gboolean authenticate;
+    GSList *Liste_sessions;
+ } Cfg_http;
+
+ struct HTTP_PER_SESSION_DATA
+  { gchar url[80];
+    gchar *post_data;
+    gint post_data_length;
+  };
+
+ struct HTTP_SESSION
+  { /*gint     type;*/
+    gchar    sid[2*EVP_MAX_MD_SIZE+1];
+    gchar    remote_name[80];
+    gchar    remote_ip[20];
+    gchar    user_agent[120];
+    gint     is_ssl;
+    struct   CMD_TYPE_UTILISATEUR *util;                                               /* Utilisateur authentifié (via HTTPS) */
+    gint     last_top;                                                                         /* Date de la derniere requete */
+  };
+
+/*************************************************** Définitions des prototypes ***********************************************/
+ extern gboolean Http_Lire_config ( void );
+/* extern gboolean Http_Traiter_request_getsyn ( struct HTTP_SESSION *session, struct MHD_Connection *connection );*/
+ extern gboolean Http_Traiter_request_getstatus ( struct lws *wsi );
+ extern gint Http_Traiter_request_getgif ( struct lws *wsi, gchar *remote_name, gchar *remote_ip, gchar *url );
+ extern gint Http_Traiter_request_getaudio ( struct lws *wsi, gchar *remote_name, gchar *remote_ip, gchar *url );
+ extern gint Http_Traiter_request_getmessage ( struct lws *wsi, struct HTTP_SESSION *session );
+ extern gint Http_Traiter_request_getui ( struct lws *wsi, gchar *remote_name, gchar *remote_ip, gchar *url );
+
+ extern struct HTTP_SESSION *Http_get_session ( struct lws *wsi, gchar *remote_name, gchar *remote_ip );
+ extern void Http_Check_sessions ( void );
+ extern void Http_Liberer_session ( struct HTTP_SESSION *session );
+ extern void Http_Close_session ( struct lws *wsi, struct HTTP_SESSION *session );
+
+ extern gint Http_Traiter_request_login ( struct HTTP_SESSION *session, struct lws *wsi, gchar *remote_name, gchar *remote_ip );
+ extern gint Http_Traiter_request_body_completion_login ( struct lws *wsi, gchar *remote_name, gchar *remote_ip );
+ 
+ /* extern gint Http_Traiter_request_getslash ( struct HTTP_SESSION *session, struct MHD_Connection *connection );
+ extern gint Http_Traiter_request_getgif ( struct MHD_Connection *connection );
+ extern gboolean Http_Traiter_request_setm ( struct HTTP_SESSION *session, struct MHD_Connection *connection );
+ extern void Http_Add_response_header ( struct MHD_Response *response );
+ extern void Http_free_liste_satellites ( void );
+ extern void Http_Check_satellites_states ( void );
+ extern void Http_Traiter_XML_set_internal ( struct HTTP_SESSION *session );*/
+#endif
+/*--------------------------------------------------------------------------------------------------------*/
