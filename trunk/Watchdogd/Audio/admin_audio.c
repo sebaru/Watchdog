1ae7d3e4aec4574f2b6ae22a5437bdfc8fa0af0a
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2017-03-24 12:05:51 +0000

    Ajout de la fonctionnalité Google Speech
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3101 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/Audio/admin_audio.c
+++ trunk/Watchdogd/Audio/admin_audio.c
@@ -1,8 +1,8 @@
-/**********************************************************************************************************/
-/* Watchdogd/Imsg/admin_imsg.c        Gestion des connexions Admin IMSG au serveur watchdog               */
-/* Projet WatchDog version 2.0       Gestion d'habitat                  sam. 28 juil. 2012 16:35:09 CEST  */
-/* Auteur: LEFEVRE Sebastien                                                                              */
-/**********************************************************************************************************/
+/******************************************************************************************************************************/
+/* Watchdogd/Imsg/admin_imsg.c        Gestion des connexions Admin IMSG au serveur watchdog                                   */
+/* Projet WatchDog version 2.0       Gestion d'habitat                                      sam. 28 juil. 2012 16:35:09 CEST  */
+/* Auteur: LEFEVRE Sebastien                                                                                                  */
+/******************************************************************************************************************************/
 /*
  * admin_imsg.c
  * This file is part of Watchdog
@@ -28,31 +28,39 @@
  #include "watchdogd.h"
  #include "Audio.h"
 
-/**********************************************************************************************************/
-/* Admin_command : Appeller par le thread admin pour traiter une commande                                 */
-/* Entrée: Le connexion d'admin, la ligne a traiter                                                       */
-/* Sortie: néant                                                                                          */
-/**********************************************************************************************************/
+/******************************************************************************************************************************/
+/* Admin_command : Appeller par le thread admin pour traiter une commande                                                     */
+/* Entrée: Le connexion d'admin, la ligne a traiter                                                                           */
+/* Sortie: néant                                                                                                              */
+/******************************************************************************************************************************/
  void Admin_command( struct CONNEXION *connexion, gchar *ligne )
   { gchar commande[128], chaine[80];
 
-    sscanf ( ligne, "%s", commande );                                /* Découpage de la ligne de commande */
+    sscanf ( ligne, "%s", commande );                                                    /* Découpage de la ligne de commande */
 
     if ( ! strcmp ( commande, "help" ) )
      { Admin_write ( connexion, "  -- Watchdog ADMIN -- Help du mode 'AUDIO'\n" );
        Admin_write ( connexion, "  dbcfg ...             - Get/Set Database Parameters\n" );
-       Admin_write ( connexion, "  tell_mp3 num          - Send message num with mp3 format\n" );
+       Admin_write ( connexion, "  tell_mp3 $num         - Send message num with mp3 format\n" );
+       Admin_write ( connexion, "  tell_google $text     - Send $text with google_speech format\n" );
        Admin_write ( connexion, "  help                  - This help\n" );
      } else
     if ( ! strcmp ( commande, "tell_mp3" ) )
      { struct CMD_TYPE_MESSAGE msg;
-       sscanf ( ligne, "%s %d", commande, &msg.num );                /* Découpage de la ligne de commande */
+       sscanf ( ligne, "%s %d", commande, &msg.num );                                    /* Découpage de la ligne de commande */
        Jouer_mp3 ( &msg );
-       g_snprintf( chaine, sizeof(chaine), " Message id %d sent (mp3)\n", msg.num );
+       g_snprintf( chaine, sizeof(chaine), " Message id %d sent with mp3\n", msg.num );
        Admin_write ( connexion, chaine );
      } else
-    if ( ! strcmp ( commande, "dbcfg" ) ) /* Appelle de la fonction dédiée à la gestion des parametres DB */
-     { if (Admin_dbcfg_thread ( connexion, NOM_THREAD, ligne+6 ) == TRUE)   /* Si changement de parametre */
+    if ( ! strcmp ( commande, "tell_google" ) )
+     { gchar texte[128];
+       sscanf ( ligne, "%s %d", commande, texte );                                       /* Découpage de la ligne de commande */
+       Jouer_google_speech ( texte );
+       g_snprintf( chaine, sizeof(chaine), " Message sent with google_speech\n" );
+       Admin_write ( connexion, chaine );
+     } else
+    if ( ! strcmp ( commande, "dbcfg" ) )                     /* Appelle de la fonction dédiée à la gestion des parametres DB */
+     { if (Admin_dbcfg_thread ( connexion, NOM_THREAD, ligne+6 ) == TRUE)                       /* Si changement de parametre */
         { gboolean retour;
           retour = Audio_Lire_config();
           g_snprintf( chaine, sizeof(chaine), " Reloading Audio Thread Parameters from Database -> %s\n",
@@ -65,4 +73,4 @@
        Admin_write ( connexion, chaine );
      }
   }
-/*--------------------------------------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------------------------------------------------------*/
