416fef1dcbf46358347156cf6af76c8c61b20ddb
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2017-03-24 08:54:56 +0000

    Retrait de la dependance vers microhttpd
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3095 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Install_SRV_Fedora.sh
+++ trunk/Install_SRV_Fedora.sh
@@ -3,7 +3,7 @@
 echo "Installing dependencies"
 dnf install -y subversion libtool automake autoconf gcc gcc-c++ redhat-rpm-config libwebsockets-devel
 dnf install -y glib2-devel bison flex readline-devel giflib-devel libgcrypt-devel rrdtool-devel
-dnf install -y libcurl-devel nut-devel mysql-devel gnokii-devel libmicrohttpd-devel 
+dnf install -y libcurl-devel nut-devel mysql-devel gnokii-devel 
 dnf install -y lirc-devel loudmouth-devel libxml2-devel libgnomeui-devel popt-devel 
 dnf install -y gtksourceview2-devel goocanvas-devel libgnomeui-devel  json-glib-devel
 dnf install pulseaudio alsa-utils alsa-firmware mpg123
