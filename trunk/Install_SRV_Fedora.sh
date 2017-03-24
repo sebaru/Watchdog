4dd1a7586278839201fc844189aac2baf6edc80e
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2017-03-24 14:46:37 +0000

    Ajout des dependances pour google_speech
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3103 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Install_SRV_Fedora.sh
+++ trunk/Install_SRV_Fedora.sh
@@ -7,6 +7,8 @@ dnf install -y libcurl-devel nut-devel mysql-devel gnokii-devel
 dnf install -y lirc-devel loudmouth-devel libxml2-devel libgnomeui-devel popt-devel 
 dnf install -y gtksourceview2-devel goocanvas-devel libgnomeui-devel  json-glib-devel
 dnf install -y pulseaudio alsa-utils alsa-firmware mpg123
+dnf install -y sox sox-plugins-freeworld
+pip3 install google_speech
 usermod -a -G audio watchdog
 
 echo "Creating systemd service"
