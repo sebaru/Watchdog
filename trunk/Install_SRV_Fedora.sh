726e55b227c83ed1ad3233f0724d338e0aafc8d7
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2017-04-03 19:00:03 +0000

    Mise à jour des dépendances dans Install_SRV.sh
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3121 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Install_SRV_Fedora.sh
+++ trunk/Install_SRV_Fedora.sh
@@ -2,7 +2,7 @@
 
 echo "Installing dependencies"
 dnf install -y subversion libtool automake autoconf gcc gcc-c++ redhat-rpm-config libwebsockets-devel
-dnf install -y glib2-devel bison flex readline-devel giflib-devel libgcrypt-devel rrdtool-devel
+dnf install -y glib2-devel bison flex readline-devel giflib-devel libgcrypt-devel
 dnf install -y libcurl-devel nut-devel mysql-devel gnokii-devel 
 dnf install -y lirc-devel loudmouth-devel libxml2-devel libgnomeui-devel popt-devel 
 dnf install -y gtksourceview2-devel goocanvas-devel libgnomeui-devel  json-glib-devel
--- trunk/Install_SRV_Raspbian.sh
+++ trunk/Install_SRV_Raspbian.sh
@@ -2,10 +2,10 @@
 
 echo "Installing dependencies"
 apt-get install -y subversion libtool automake autoconf gcc
-apt-get install -y libglib2.0-dev bison flex libreadline-dev libgif-dev libgcrypt11-dev librrd-dev
+apt-get install -y libglib2.0-dev bison flex libreadline-dev libgif-dev libgcrypt11-dev libwebsockets-dev
 apt-get install -y libupsclient1-dev libcurl4-gnutls-dev libssl-dev libmysqlclient-dev libgnokii-dev
-apt-get install -y liblircclient-dev libloudmouth1-dev libmicrohttpd-dev libpopt-dev
-apt-get install -y libgnomeui-dev libgoocanvas-dev libgtksourceview2.0-dev 
+apt-get install -y liblircclient-dev libloudmouth1-dev libpopt-dev
+apt-get install -y libgnomeui-dev libgoocanvas-dev libgtksourceview2.0-dev libjson-glib-dev 
 
 #echo "Creating systemd service"
 #cp Watchdogd.service /lib/systemd/system/
