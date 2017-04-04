908d927c0da163297959716e93115289786a5449
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2017-04-04 05:56:44 +0000

    Simplification des scripts d'installation
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3122 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Install_SRV_Fedora.sh
+++ trunk/Install_SRV_Fedora.sh
@@ -9,17 +9,4 @@ dnf install -y gtksourceview2-devel goocanvas-devel libgnomeui-devel  json-glib-
 dnf install -y pulseaudio alsa-utils alsa-firmware mpg123
 dnf install -y sox sox-plugins-freeworld
 pip3 install google_speech
-usermod -a -G audio watchdog
-
-echo "Creating systemd service"
-ln -s /usr/local/etc/Watchdogd.service /etc/systemd/system/Watchdogd.service
-systemctl daemon-reload
-systemctl enable Watchdogd.service
-echo "done."
-
-echo "Creating etc conf file"
-ln -s /usr/local/etc/watchdogd.conf.sample /etc/watchdogd.conf.sample
-ln -s /usr/local/etc/watchdogd-httpd.conf.sample /etc/watchdogd-httpd.conf.sample
-systemctl daemon-reload
-echo "done."
 
--- trunk/Install_SRV_Raspbian.sh
+++ trunk/Install_SRV_Raspbian.sh
@@ -7,7 +7,3 @@ apt-get install -y libupsclient1-dev libcurl4-gnutls-dev libssl-dev libmysqlclie
 apt-get install -y liblircclient-dev libloudmouth1-dev libpopt-dev
 apt-get install -y libgnomeui-dev libgoocanvas-dev libgtksourceview2.0-dev libjson-glib-dev 
 
-#echo "Creating systemd service"
-#cp Watchdogd.service /lib/systemd/system/
-#systemctl enable Watchdogd.service
-echo "done."
