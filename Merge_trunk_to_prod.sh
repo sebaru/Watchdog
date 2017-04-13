c373fe267f316142c508a7468e408a9eb33e2731
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2017-04-13 16:02:18 +0000

    Correction script de merging
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3137 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Merge_trunk_to_prod.sh
+++ Merge_trunk_to_prod.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 svn update
-from=`svnversion -n -c | awk -F: '{print $2}'`
-dest=`svnversion -n`
+from=`svnversion -n -c prod | awk -F: '{print $2}'`
+dest=`svnversion -n trunk`
 svn merge https://svn.abls-habitat.fr/repo/Watchdog/trunk .
 Changes=`svn log ^/trunk -r $from:$dest -v`
 svn ci -m $Changes
