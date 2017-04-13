67fef2f6f60055b689e219914121a4037fdab864
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2017-04-13 15:56:35 +0000

    Retrait du repertoire prod pour repartir du trunk
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3134 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- prod/Merge_from_trunk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-svn update
-from=`svnversion -n -c | awk -F: '{print $2}'`
-dest=`svnversion -n`
-svn merge https://svn.abls-habitat.fr/repo/Watchdog/trunk .
-Changes=`svn log ^/trunk -r $from:$dest -v`
-svn ci -m $Changes
--- prod/ShowDiff.sh
+++
@@ -1,2 +0,0 @@
-#/bin/sh
-svn diff --old ^/prod --new ^/trunk | more
