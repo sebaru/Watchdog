b931a214b40c090a893419912d95a93e23962b5f
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2017-04-13 15:51:23 +0000

    Merge from trunk
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3133 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- prod/Merge_from_trunk.sh
+++ prod/Merge_from_trunk.sh
@@ -3,4 +3,5 @@ svn update
 from=`svnversion -n -c | awk -F: '{print $2}'`
 dest=`svnversion -n`
 svn merge https://svn.abls-habitat.fr/repo/Watchdog/trunk .
-svn ci -m `svn log ^/trunk -r $from:$dest -v`
+Changes=`svn log ^/trunk -r $from:$dest -v`
+svn ci -m $Changes
