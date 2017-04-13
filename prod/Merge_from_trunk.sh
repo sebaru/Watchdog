da7ecd837f67bf091bae801981003d008c71f8bc
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2017-04-13 15:45:59 +0000

    Intégration du svnlog lors du merge du trunk
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3132 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- prod/Merge_from_trunk.sh
+++ prod/Merge_from_trunk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 svn update
-svn merge https://svn.abls-habitat.fr/repo/Watchdog/trunk .
 from=`svnversion -n -c | awk -F: '{print $2}'`
 dest=`svnversion -n`
+svn merge https://svn.abls-habitat.fr/repo/Watchdog/trunk .
 svn ci -m `svn log ^/trunk -r $from:$dest -v`
