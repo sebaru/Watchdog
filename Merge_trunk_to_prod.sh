d034c07b14760b696ad0743b5fc83e35bbfdd061
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2017-04-14 10:24:19 +0000

    Changement du message de commit pour le merge trunk
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3140 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Merge_trunk_to_prod.sh
+++ Merge_trunk_to_prod.sh
@@ -3,5 +3,5 @@ svn update
 from=`svnversion -n -c prod | awk -F: '{print $2}'`
 dest=`svnversion -n trunk`
 svn merge https://svn.abls-habitat.fr/repo/Watchdog/trunk prod
-Changes=`svn log ^/trunk -r $from:$dest -v`
-svn ci -m $Changes
+Changes=`svn log ^/trunk -r $from:$dest -v | egrep 'FCT:|COR:' `
+svn ci -m "Merging changes from PROD Rev $from to TRUNK Rev $dest -> $Changes"
