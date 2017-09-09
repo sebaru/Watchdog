7bbb95ab037be0ec6c304ea738888ecd2a3d6a5e
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2017-09-09 07:34:13 +0000

    Ajout de la prise en compte des retours chariots
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3296 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- Merge_trunk_to_prod.sh
+++ Merge_trunk_to_prod.sh
@@ -4,5 +4,5 @@ from=`svnversion -n -c prod | awk -F: '{print $2}'`
 dest=`svnversion -n trunk`
 svn merge https://svn.abls-habitat.fr/repo/Watchdog/trunk prod
 Changes=`svn log ^/trunk -r $from:$dest -v | egrep 'FCT:|COR:' `
-echo $Changes
+echo -e $Changes
 svn ci -m "Merging changes from PROD Rev $from to TRUNK Rev $dest -> $Changes"
