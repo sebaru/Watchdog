46806133ebab8dd8b655d219508695664c77cad0
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2018-02-15 10:30:52 +0000

    Ajout de log
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3487 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/Sms/Sms.c
+++ trunk/Watchdogd/Sms/Sms.c
@@ -614,8 +614,10 @@
 /********************************************************* Envoi de SMS *******************************************************/
        if (Recv_zmq ( zmq_admin, &buffer, sizeof(buffer)) > 0 )                           /* As-t'on recu un paquet d'admin ? */
         { gchar *response;
+          Info_new( Config.log, Cfg_sms.lib->Thread_debug, LOG_INFO, "%s: Recu commande admin %s", __func__, buffer );
           response = Admin_response ( buffer );
           Send_zmq ( zmq_admin, response, strlen(response)+1 );
+          Info_new( Config.log, Cfg_sms.lib->Thread_debug, LOG_DEBUG, "%s: Response admin %s", __func__, response );
           g_free(response);
         }
 
