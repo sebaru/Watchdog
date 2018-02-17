f30df0e974a9a7501ad72829648976dfbfa83e41
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2018-02-17 15:28:06 +0000

    Ajout de plus de log
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3499 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/Audio/Audio.c
+++ trunk/Watchdogd/Audio/Audio.c
@@ -276,8 +276,7 @@
         }
        else
         { Info_new( Config.log, Cfg_audio.lib->Thread_debug, LOG_DEBUG,
-                   "%s : test", __func__ );
-                   //Msg Audio non envoye num=%d (histo->msg.audio=%d)", __func__, histo->msg.num, histo->msg.audio );
+                   "%s : Msg Audio non envoye num=%d (histo->msg.audio=%d)", __func__, histo->msg.num, histo->msg.audio );
         }
      }
     Close_zmq ( zmq_msg );
