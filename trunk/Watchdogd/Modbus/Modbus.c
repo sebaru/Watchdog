31e6a5871a9d2430dc0e7ac4baef31c7360b20f4
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2018-09-09 14:42:10 +0000

    Modbus : changement de separateur dans les affectations dynamiques
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3722 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/Modbus/Modbus.c
+++ trunk/Watchdogd/Modbus/Modbus.c
@@ -763,7 +763,7 @@
      }
 
 /******************************* Recherche des event text EA a raccrocher aux bits internes ***********************************/
-    g_snprintf( critere, sizeof(critere),"%s_AI%%", module->modbus.libelle ); 
+    g_snprintf( critere, sizeof(critere),"%s:AI%%", module->modbus.libelle ); 
     if ( ! Recuperer_mnemo_baseDB_by_event_text ( &db, NOM_THREAD, critere ) )
      { Info_new( Config.log, Cfg_modbus.lib->Thread_debug, LOG_ERR, "%s: Error searching Database for '%s'", __func__, critere ); }
     else while ( (mnemo = Recuperer_mnemo_baseDB_suite( &db )) != NULL)
@@ -772,7 +772,7 @@
        if ( mnemo->type == MNEMO_ENTREE_ANA )
         { gchar debut[80];
           gint num;
-          if ( sscanf ( mnemo->ev_text, "%s_AI%d", debut, &num ) == 2 )                       /* Découpage de la ligne ev_text */
+          if ( sscanf ( mnemo->ev_text, "%[^:]:AI%d", debut, &num ) == 2 )                       /* Découpage de la ligne ev_text */
            { if (num<module->nbr_entree_ana)
               { Dls_data_set_AI ( mnemo->dls_tech_id, mnemo->acronyme, &module->AI[num], 0.0 ); }
              else Info_new( Config.log, Cfg_modbus.lib->Thread_debug, LOG_WARNING, "%s: event '%s': num %d out of range '%d'", __func__,
@@ -784,7 +784,7 @@
        g_free(mnemo);
      }
 /******************************* Recherche des event text EA a raccrocher aux bits internes ***********************************/
-    g_snprintf( critere, sizeof(critere),"%s_DI%%", module->modbus.libelle ); 
+    g_snprintf( critere, sizeof(critere),"%s:DI%%", module->modbus.libelle ); 
     if ( ! Recuperer_mnemo_baseDB_by_event_text ( &db, NOM_THREAD, critere ) )
      { Info_new( Config.log, Cfg_modbus.lib->Thread_debug, LOG_ERR, "%s: Error searching Database for '%s'", __func__, critere ); }
     else while ( (mnemo = Recuperer_mnemo_baseDB_suite( &db )) != NULL)
@@ -793,7 +793,7 @@
        if ( mnemo->type == MNEMO_ENTREE )
         { gchar debut[80];
           gint num;
-          if ( sscanf ( mnemo->ev_text, "%s_DI%d", debut, &num ) == 2 )                       /* Découpage de la ligne ev_text */
+          if ( sscanf ( mnemo->ev_text, "%[^:]:DI%d", debut, &num ) == 2 )                       /* Découpage de la ligne ev_text */
            { if (num<module->nbr_entree_tor)
               { Dls_data_set_bool ( mnemo->acronyme, mnemo->dls_tech_id, (gboolean **)&module->DI[cpt], FALSE ); }
              else Info_new( Config.log, Cfg_modbus.lib->Thread_debug, LOG_WARNING, "%s: event '%s': num %d out of range '%d'", __func__,
