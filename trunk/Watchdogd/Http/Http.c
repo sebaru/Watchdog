7beb656ce84f7cb678357690e18e5309fc5b5b98
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2018-11-18 18:39:05 +0000

    Correction detection webservice /reload/
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3796 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/Http/Http.c
+++ trunk/Watchdogd/Http/Http.c
@@ -387,6 +387,36 @@
                 { g_snprintf( pss->url, sizeof(pss->url), "/postfile" );
                   return(0);
                 }
+               else if ( ! strcasecmp( url, "/dls_reload" ) )
+                { Info_new( Config.log, Cfg_http.lib->Thread_debug, LOG_NOTICE, "%s: Reloading DLS", __func__ );
+                  Partage->com_dls.Thread_reload = TRUE;
+                  Http_Send_response_code ( wsi, HTTP_200_OK );
+                  return(1);
+                }
+               else if ( ! strncasecmp( url, "/reload/", 8 ) )
+                { gchar *target = url+8;
+                  GSList *liste;
+                  Info_new( Config.log, Cfg_http.lib->Thread_debug, LOG_NOTICE,
+                            "%s: Reloading start for %s", __func__, target );
+                  liste = Partage->com_msrv.Librairies;                            /* Parcours de toutes les librairies */
+                  while(liste)
+                   { struct LIBRAIRIE *lib = liste->data;
+                     if ( ! strcmp( target, lib->admin_prompt ) )
+                      { if (lib->Thread_run == FALSE)
+                         { Info_new( Config.log, Cfg_http.lib->Thread_debug, LOG_NOTICE,
+                                    "%s: reloading %s -> Library found but not started.", __func__, target );
+                         }    
+                        else
+                         { Info_new( Config.log, Cfg_http.lib->Thread_debug, LOG_NOTICE,
+                                    "%s: reloading %s -> Library found. Sending Reload.", __func__, target );
+                           lib->Thread_reload = TRUE;
+                         }    
+                      }
+                     liste = g_slist_next(liste);
+                   }
+                  Http_Send_response_code ( wsi, HTTP_200_OK );
+                  return(1);
+                }
                else                                                                                             /* Par défaut */
                 { gchar token_id[12];
                   const gchar *id_s;
@@ -404,36 +434,6 @@
                      else if ( ! strcasecmp( url, "/dlsdelete" ) )
                       { Info_new( Config.log, Cfg_http.lib->Thread_debug, LOG_NOTICE, "%s: Delete DLS %d", __func__, id );
                         Decharger_plugin_by_id( id );
-                        Http_Send_response_code ( wsi, HTTP_200_OK );
-                        return(1);
-                      }
-                     else if ( ! strcasecmp( url, "/dls_reload" ) )
-                      { Info_new( Config.log, Cfg_http.lib->Thread_debug, LOG_NOTICE, "%s: Reloading DLS", __func__ );
-                        Partage->com_dls.Thread_reload = TRUE;
-                        Http_Send_response_code ( wsi, HTTP_200_OK );
-                        return(1);
-                      }
-                     else if ( ! strncasecmp( url, "/reload/", 8 ) )
-                      { gchar *target = url+8;
-                        GSList *liste;
-                        Info_new( Config.log, Cfg_http.lib->Thread_debug, LOG_NOTICE,
-                                  "%s: Reloading start for %s", __func__, target );
-                        liste = Partage->com_msrv.Librairies;                            /* Parcours de toutes les librairies */
-                        while(liste)
-                         { struct LIBRAIRIE *lib = liste->data;
-                           if ( ! strcmp( target, lib->admin_prompt ) )
-                            { if (lib->Thread_run == FALSE)
-                               { Info_new( Config.log, Cfg_http.lib->Thread_debug, LOG_NOTICE,
-                                          "%s: reloading %s -> Library found but not started.", __func__, target );
-                               }    
-                              else
-                               { Info_new( Config.log, Cfg_http.lib->Thread_debug, LOG_NOTICE,
-                                          "%s: reloading %s -> Library found. Sending Reload.", __func__, target );
-                                 lib->Thread_reload = TRUE;
-                               }    
-                            }
-                           liste = g_slist_next(liste);
-                         }
                         Http_Send_response_code ( wsi, HTTP_200_OK );
                         return(1);
                       }
