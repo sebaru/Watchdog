cbc1cb9f369df41c350cdc88c40eb110fd24ffd7
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2018-12-28 07:14:39 +0000

    Correction pointeur bool
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3846 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/Include/Module_dls.h
+++ trunk/Watchdogd/Include/Module_dls.h
@@ -54,7 +54,7 @@
  extern gboolean Dls_data_get_bool      ( gchar *tech_id, gchar *acronyme, gpointer *bool_p );
  extern gboolean Dls_data_get_bool_up   ( gchar *tech_id, gchar *acronyme, gpointer *bool_p );
  extern gboolean Dls_data_get_bool_down ( gchar *tech_id, gchar *acronyme, gpointer *bool_p );
- extern void     Dls_data_set_bool      ( gchar *tech_id, gchar *acronyme, gpointer *data_p, gboolean valeur );
+ extern void     Dls_data_set_bool      ( gchar *tech_id, gchar *acronyme, gpointer *bool_p, gboolean valeur );
  extern void     Dls_data_set_MSG       ( gchar *tech_id, gchar *acronyme, gpointer *msg_p, gboolean etat );
  extern void     Dls_data_set_tempo     ( gchar *tech_id, gchar *acronyme, gpointer *tempo_p, gboolean etat,
                                           gint delai_on, gint min_on, gint max_on, gint delai_off, gint random);
