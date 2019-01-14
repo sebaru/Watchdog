cabf3b70385037e15161402b84f1bfff96874e41
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2019-01-14 13:09:44 +0000

    Voice : Le delai d'inhib est maintenant en base et paramétrable
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3898 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/Voice/Voice.c
+++ trunk/Watchdogd/Voice/Voice.c
@@ -50,6 +50,7 @@
 
     Cfg_voice.lib->Thread_debug = FALSE;                                                       /* Settings default parameters */
     Cfg_voice.enable            = FALSE; 
+    Cfg_voice.delai_inhib       = 300; 
     g_snprintf( Cfg_voice.audio_device,  sizeof(Cfg_voice.audio_device),  "default" );
     g_snprintf( Cfg_voice.key_words,     sizeof(Cfg_voice.key_words),     "dis moi jolie maison" );
     g_snprintf( Cfg_voice.gain_control,  sizeof(Cfg_voice.gain_control),  "noise" );
@@ -76,6 +77,8 @@
         { g_snprintf( Cfg_voice.vad_threshold, sizeof(Cfg_voice.vad_threshold), "%s", valeur ); }
        else if ( ! g_ascii_strcasecmp ( nom, "debug" ) )
         { if ( ! g_ascii_strcasecmp( valeur, "true" ) ) Cfg_voice.lib->Thread_debug = TRUE;  }
+       else if ( ! g_ascii_strcasecmp ( nom, "delai_inhib" ) )
+        { Cfg_voice.delai_inhib = atoi(valeur); }
        else
         { Info_new( Config.log, Cfg_voice.lib->Thread_debug, LOG_NOTICE,
                    "%s: Unknown Parameter '%s'(='%s') in Database", __func__, nom, valeur );
@@ -301,8 +304,10 @@ reload:
        commande_vocale[retour-1-strlen(" merci")]=0;                                                 /*Caractere NULL d'arret */
        evenement = commande_vocale + strlen(Cfg_voice.key_words) + 1;
        Info_new( Config.log, Cfg_voice.lib->Thread_debug, LOG_ERR, "%s: recu = %s (last_evt=%d, top=%d)", __func__, evenement, last_evt, Partage->top );
-       if (last_evt + 300 > Partage->top)
-        { Info_new( Config.log, Cfg_voice.lib->Thread_debug, LOG_ERR, "%s: recu = %s but too fast !", __func__, evenement );
+       if (Partage->top <= last_evt + Cfg_voice.delai_inhib)
+        { Info_new( Config.log, Cfg_voice.lib->Thread_debug, LOG_ERR,
+                    "%s: recu = %s but too fast. Last_evt=%d. Next evt not before %d !", __func__,
+                    evenement, last_evt, last_evt + Cfg_voice.delai_inhib );
           continue;
         }
        last_evt = Partage->top;
