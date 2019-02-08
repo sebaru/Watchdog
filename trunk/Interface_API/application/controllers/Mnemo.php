0630909f317bd63838ea222dd820f835f9121c03
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2019-02-08 19:42:35 +0000

    API: Ajout de detection d'erreur
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3952 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Interface_API/application/controllers/Mnemo.php
+++ trunk/Interface_API/application/controllers/Mnemo.php
@@ -73,15 +73,18 @@ class Mnemo extends Admin_Controller {
      }*/
 
     $input = json_decode(file_get_contents('php://input'));
-    if ($input==NULL) { echo json_encode( array( "success" => "false", "error" => "Parsing Error !" ) ); exit(); }
+    if (!isset($input)) { echo json_encode( array( "success" => "false", "error" => "Parsing Error !" ) ); exit(); }
+
+    $mnemo = $this->Mnemo_model->get($input->id);
+    if (!isset($mnemo)) { echo json_encode( array( "success" => "false", "error" => "Mnemo unknown !" ) ); exit(); }
 
     $data = array();
-    if ($input->libelle)   $data['libelle']   = $input->libelle;
-    if ($input->ev_host)   $data['ev_host']   = $input->ev_host;
-    if ($input->ev_thread) $data['ev_thread'] = strtoupper($input->ev_thread);
-    if ($input->ev_host)   $data['ev_text']   = $input->ev_text;
+    if (isset($input->libelle))   $data['libelle']   = $input->libelle;
+    if (isset($input->ev_host))   $data['ev_host']   = $input->ev_host;
+    if (isset($input->ev_thread)) $data['ev_thread'] = strtoupper($input->ev_thread);
+    if (isset($input->ev_host))   $data['ev_text']   = $input->ev_text;
 
-    if($this->Mnemo_model->update($input->id, $data))
+    if($this->Mnemo_model->update($mnemo->id, $data))
      { echo json_encode( array( "success" => "true", "Mnemo" => "Updated !" ) );
        if($mnemo->ev_thread=="VOICE"  || $data['ev_thread']=="VOICE")  { $this->wtd_webservice->send('/reload/voice'); }
        if($mnemo->ev_thread=="MODBUS" || $data['ev_thread']=="MODBUS") { $this->wtd_webservice->send('/reload/modbus'); }
