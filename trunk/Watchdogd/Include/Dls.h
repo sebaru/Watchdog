6edb68ff5bac2fd2b195c57b3c18396cc4253e9d
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2019-02-27 16:08:09 +0000

    DLS: Changement de la prise en charge des sorties qui bagottent
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@3991 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/Include/Dls.h
+++ trunk/Watchdogd/Include/Dls.h
@@ -100,7 +100,6 @@
  struct SORTIE_TOR                                                                             /* Définition d'une sortie TOR */
   { gchar etat;                                                                                   /* Etat de la sortie 0 ou 1 */
     gint last_change;                                                                    /* Date du dernier changement d'etat */
-    gint changes;                               /* Compte le nombre de changes afin de ne pas depasser une limite par seconde */
   };
 
  struct DLS_BOOL
