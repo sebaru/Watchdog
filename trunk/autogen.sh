101267bac8abd4339808d07fad5007a50bc44f72
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2019-03-28 01:21:18 +0000

    FCT: Création de fichier Makefile spécifiques fedora et raspbian
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@4041 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -1,6 +1,22 @@
 #! /bin/sh
 
 svn update
+osFedora=`grep "NAME=Fedora" /etc/os-release`
+osRaspbian=`grep "ID=raspbian" /etc/os-release`
+
+if [ -n "$osFedora" ]; then
+  echo "Compiling for Fedora !"
+  cp Makefile.am.fedora Makefile.am
+  cp Watchdogd/Makefile.am.fedora Watchdogd/Makefile.am
+
+fi
+
+if [ -n "$osRaspbian" ]; then
+  echo "Compiling for Raspbian !"
+  cp Makefile.am.raspbian Makefile.am
+  cp Watchdogd/Makefile.am.raspbian Watchdogd/Makefile.am
+fi
+
 autoheader
 libtoolize
 aclocal --force
