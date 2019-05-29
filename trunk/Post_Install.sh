1d70ee801648fea1ec2e1fae52ba2808f85fefea
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2019-05-29 17:23:49 +0000

    Simplification PostInstall via readline
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@4162 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Post_Install.sh
+++ trunk/Post_Install.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 echo "Creating systemd service"
-wtd_home=/var/lib/watchdog
 
 echo "Quel user fera tourner Watchdog ?"
-read wtd_user
+read -p "User: " wtd_user
 
-echo "Dois-je installé la Database (oui/non) ?"
-read database
+echo "Dois-je installer la Database (oui/non) ?"
+read -p "Création DB: " database
+
+if [ "$wtd_user" = "watchdog" ]
+	then
+		wtd_home=~$wtd_user
+	else
+		wtd_home=~$wtd_user/.watchdog
+fi
 
 echo "Installation in standalone mode in $wtd_home for $wtd_user"
 sleep 5
@@ -22,10 +28,10 @@ sleep 2
 
 echo "Copying data files"
 sudo mkdir -p $wtd_home
-sudo chown $wtd_user $wtd_home
-mkdir -p $wtd_home/Son
-mkdir -p $wtd_home/Dls
-cp -r Son/* $wtd_home/Son
+sudo mkdir -p $wtd_home/Son
+sudo mkdir -p $wtd_home/Dls
+sudo cp -r Son/* $wtd_home/Son
+sudo chown -R $wtd_user:users $wtd_home
 echo "done."
 sleep 2
 
@@ -35,11 +41,8 @@ CONFFILE=/etc/watchdogd.conf
 if [ ! -f $CONFFILE ]
  then
     echo "Creating New watchdog database passwd"
-    if [ "$database" = "oui" ]
-    then 
-      NEWPASSWORD=`openssl rand -base64 32`
-      sed "/usr/local/etc/watchdogd.conf.sample" -e "s#dbpasstobechanged#$NEWPASSWORD#g" | \
-    fi
+    NEWPASSWORD=`openssl rand -base64 32`
+    sed "/usr/local/etc/watchdogd.conf.sample" -e "s#dbpasstobechanged#$NEWPASSWORD#g" | \
     sed -e "s#usertobechanged#$wtd_user#g" | \
     sudo tee "$CONFFILE" > /dev/null
     if [ "$database" = "oui" ]
@@ -50,7 +53,6 @@ if [ ! -f $CONFFILE ]
     fi
 fi
 echo "done."
-fi
 
 echo "Starting Watchdog"
 	sudo systemctl start Watchdogd.service
