20480158ad194771cbdf0c4be7d50d3e3bd5e51b
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2019-08-07 09:38:46 +0000

    Correction requete SQL erronée
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@4272 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/Histo_msgs.c
+++ trunk/Watchdogd/Histo_msgs.c
@@ -147,7 +147,7 @@
                 " INNER JOIN %s as dls ON dls.id = msg.dls_id"
                 " INNER JOIN %s as syn ON syn.id = dls.syn_id"
                 " INNER JOIN %s as parent_syn ON parent_syn.id = syn.parent_id"
-                " WHERE alive = 1 ORDER BY histo.date_create_sec, histo.date_create_usec",
+                " WHERE alive = 1 ORDER BY histo.date_create",
                 NOM_TABLE_HISTO_MSGS, NOM_TABLE_MSG, NOM_TABLE_DLS, NOM_TABLE_SYNOPTIQUE, NOM_TABLE_SYNOPTIQUE        /* From */
               );
 
