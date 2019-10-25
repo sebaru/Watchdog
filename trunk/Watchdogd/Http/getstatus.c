fad5d58ab34c8b623e6c347d6f0c437ca5439caf
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2019-10-25 15:39:59 +0000

    FCT: Ajout du 'run_as' dans le status
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@4380 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/Http/getstatus.c
+++ trunk/Watchdogd/Http/getstatus.c
@@ -60,6 +60,7 @@
     Json_add_string ( builder, "version",  VERSION );
     Json_add_string ( builder, "instance", g_get_host_name() );
     Json_add_bool   ( builder, "instance_is_master", Config.instance_is_master );
+    Json_add_string ( builder, "runs_as", Config.run_as );
 
     temps = localtime( (time_t *)&Partage->start_time );
     if (temps) { strftime( date, sizeof(date), "%F %T", temps ); }
