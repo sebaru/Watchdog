6a56867e968a6ce54f08240579db16ba1454b284
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2020-03-29 16:02:02 +0000

    Changement d'année du copyright
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@4513 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/Mnemo_CI.c
+++ trunk/Watchdogd/Mnemo_CI.c
@@ -7,7 +7,7 @@
  * Mnemo_CI.c
  * This file is part of Watchdog
  *
- * Copyright (C) 2010-2019 - Sebastien Lefevre
+ * Copyright (C) 2010-2020 - Sebastien Lefevre
  *
  * Watchdog is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
