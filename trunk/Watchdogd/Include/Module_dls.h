e0df10871bbb501ba610daf00a7eba1ebd66cc7b
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2020-04-14 13:37:20 +0000

    FCT: DLS: Les Sorties TOR dynamiques ont leurs propres tableau: DLS_DO
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@4573 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/Include/Module_dls.h
+++ trunk/Watchdogd/Include/Module_dls.h
@@ -61,7 +61,7 @@
  extern gboolean Dls_data_get_DI        ( gchar *tech_id, gchar *acronyme, gpointer *di_p );
  extern gboolean Dls_data_get_DI_up     ( gchar *tech_id, gchar *acronyme, gpointer *di_p );
  extern gboolean Dls_data_get_DI_down   ( gchar *tech_id, gchar *acronyme, gpointer *di_p );
- extern void     Dls_data_set_DO        ( gchar *tech_id, gchar *acronyme, gpointer *di_p, gboolean valeur );
+ extern void     Dls_data_set_DO        ( gchar *tech_id, gchar *acronyme, gpointer *dout_p, gboolean valeur );
  extern void     Dls_data_set_MSG       ( gchar *tech_id, gchar *acronyme, gpointer *msg_p, gboolean etat );
  extern void     Dls_data_set_tempo     ( gchar *tech_id, gchar *acronyme, gpointer *tempo_p, gboolean etat,
                                           gint delai_on, gint min_on, gint max_on, gint delai_off, gint random);
