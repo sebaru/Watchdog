8442d6e83668758cd3548d52eb165cab8fb57200
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2020-05-03 08:12:42 +0000

    CLIENT: Ajout dependance goocanvas sur debian
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@4662 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Install_SRV_Debian.sh
+++ trunk/Install_SRV_Debian.sh
@@ -8,5 +8,5 @@ apt install -y libupsclient-dev libcurl4-gnutls-dev libssl-dev default-libmysqlc
 apt install -y liblircclient-dev libpopt-dev libssl-dev libmariadbclient-dev libzmq3-dev libwebsockets-dev
 apt install -y sox libsox-fmt-all python3-pip mpg123
 apt install -y libxml2-dev libjson-glib-dev libmosquitto-dev vlc 
-apt install -y libgtk-3-dev libsoup2.4-dev
+apt install -y libgtk-3-dev libsoup2.4-dev libgoocanvas-2.0-dev
 pip3 install gTTS
