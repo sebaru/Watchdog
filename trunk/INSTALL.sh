69d1fa849b4dca6ca74dd303fc3a62473e4e5615
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2020-06-04 15:35:13 +0000

    Ajout du sudo
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@4756 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/INSTALL.sh
+++ trunk/INSTALL.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 
-SOCLE = `grep "^ID" /etc/os-release | cut -f 2 -d '='`
-USER  =`whoami`
+SOCLE=`grep "^ID" /etc/os-release | cut -f 2 -d '='`
+USER=`whoami`
 
 if [ "$SOCLE" = "fedora" ]
  then
   echo "Installing Fedora dependencies"
-  dnf update
-  dnf install -y subversion libtool automake autoconf gcc gcc-c++ redhat-rpm-config
-  dnf install -y glib2-devel bison flex readline-devel giflib-devel libgcrypt-devel
-  dnf install -y libcurl-devel nut-devel mariadb-devel zeromq-devel
-  dnf install -y gtk3-devel goocanvas2-devel popt-devel libsoup-devel
-  dnf install -y gtksourceview2-devel goocanvas-devel json-glib-devel gammu-devel
-  dnf install -y vlc alsa-utils alsa-firmware
-  dnf install -y mpg123 sox mosquitto-devel
+  sudo dnf update
+  sudo dnf install -y subversion libtool automake autoconf gcc gcc-c++ redhat-rpm-config
+  sudo dnf install -y glib2-devel bison flex readline-devel giflib-devel libgcrypt-devel
+  sudo dnf install -y libcurl-devel nut-devel mariadb-devel zeromq-devel
+  sudo dnf install -y gtk3-devel goocanvas2-devel popt-devel libsoup-devel
+  sudo dnf install -y gtksourceview2-devel goocanvas-devel json-glib-devel gammu-devel
+  sudo dnf install -y vlc alsa-utils alsa-firmware
+  sudo dnf install -y mpg123 sox mosquitto-devel
 fi
 
 if [ "$SOCLE" = "debian" ]
  then
   echo "Installing debian dependencies"
-  apt update
-  apt install -y subversion libtool automake autoconf gcc git cmake
-  apt install -y libglib2.0-dev bison flex libreadline-dev libgif-dev libgcrypt20-dev
-  apt install -y libupsclient-dev libcurl4-gnutls-dev libssl-dev default-libmysqlclient-dev libstrophe-dev libgammu-dev
-  apt install -y liblircclient-dev libpopt-dev libssl-dev libmariadbclient-dev libzmq3-dev
-  apt install -y sox libsox-fmt-all python3-pip mpg123
-  apt install -y libjson-glib-dev libmosquitto-dev vlc
-  apt install -y libgtk-3-dev libsoup2.4-dev libgoocanvas-2.0-dev
+  sudo apt update
+  sudo apt install -y subversion libtool automake autoconf gcc git cmake
+  sudo apt install -y libglib2.0-dev bison flex libreadline-dev libgif-dev libgcrypt20-dev
+  sudo apt install -y libupsclient-dev libcurl4-gnutls-dev libssl-dev default-libmysqlclient-dev libstrophe-dev libgammu-dev
+  sudo apt install -y liblircclient-dev libpopt-dev libssl-dev libmariadbclient-dev libzmq3-dev
+  sudo apt install -y sox libsox-fmt-all python3-pip mpg123
+  sudo apt install -y libjson-glib-dev libmosquitto-dev vlc
+  sudo apt install -y libgtk-3-dev libsoup2.4-dev libgoocanvas-2.0-dev
 fi
 
 echo "L'instance est-elle master ? (oui/non)"
