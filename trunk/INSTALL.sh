facba6fababd7b1b8ac7c308076b1272ac1a172f
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2020-06-04 17:22:00 +0000

    Ajout des permissions sur le repertoire d'interface web 2/2
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@4764 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/INSTALL.sh
+++ trunk/INSTALL.sh
@@ -99,8 +99,8 @@ if [ "$web" = "oui" ]
      sudo a2enmod rewrite
      sudo a2dissite 000-default
      sudo systemctl reload apache2
-     sudo chmod www-data /var/www/html/WEB
-     sudo -u www-data https://svn.abls-habitat.fr/repo/Watchdog/trunk/Interface_WEB /var/www/html/WEB
+     sudo chown www-data /var/www/html/WEB
+     sudo -u www-data svn https://svn.abls-habitat.fr/repo/Watchdog/trunk/Interface_WEB /var/www/html/WEB
     fi
 
 
