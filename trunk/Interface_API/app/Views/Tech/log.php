55377dc85f3d3493d0bdff6ccdd6ef3006bf7ed8
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2020-07-06 19:30:02 +0000

    Interface_API: Ajout des php et js pour la page log
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@4807 9f22af27-4b39-4cfe-9f85-4bed9fef4693

---
+++ trunk/Interface_API/app/Views/Tech/log.php
@@ -0,0 +1,12 @@
+<div class="container-fluid">
+
+  <table id="idTableLog">
+    <thead class="thead-dark">
+				</thead>
+			 <tbody>
+    </tbody>
+  </table>
+
+<script src="<?php echo base_url('js/log.js')?>" type="text/javascript"></script>
+<!-- Container -->
+</div>
