716cd14a0ef2c5d1e2a26b8f49af41080c1f82f4
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2020-07-31 19:21:56 +0000

    API: Ajout page d'edition DLS 2/2
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@4853 9f22af27-4b39-4cfe-9f85-4bed9fef4693

---
+++ trunk/Interface_API/public/js/dls_source.js
@@ -0,0 +1,34 @@
+ document.addEventListener('DOMContentLoaded', Load_page, false);
+
+ function Go_to_mnemo ()
+  { vars = window.location.pathname.split('/');
+    Redirect ( "/tech/mnemos/"+vars[3] );
+  }
+ function Go_to_run ()
+  { vars = window.location.pathname.split('/');
+    Redirect ( "/tech/run/"+vars[3] );
+  }
+
+/********************************************* Appelé au chargement de la page ************************************************/
+ function Load_page ()
+  { vars = window.location.pathname.split('/');
+    var SourceCode = CodeMirror.fromTextArea( document.getElementById("idSourceCode"), { lineNumbers: true } );
+    SourceCode.setSize( null, "100%");
+
+    $("#idSourceTitle").text(vars[3]);
+
+    var xhr = new XMLHttpRequest;
+    xhr.open('GET', "/api/dls/source/"+vars[3], true);
+    xhr.onreadystatechange = function()
+     { if ( xhr.readyState != 4 ) return;
+       if (xhr.status != 200)
+        { Show_Error ( xhr.statusText );
+          return;
+        }
+       var Response = JSON.parse(xhr.responseText);
+       SourceCode.getDoc().setValue(Response.sourcecode);
+       $("#idErrorLog").html(Response.errorlog.replace(/(?:\r\n|\r|\n)/g, '<br>'));
+     };
+    xhr.send();
+
+  }
