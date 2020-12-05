25eefd2fe9f2c660668720f527d97e45c5cb1a65
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2020-12-05 17:50:58 +0000

    FCT: IHM: Début de l'atelier
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@5193 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/IHM/Tech/atelier.php
+++ trunk/Watchdogd/IHM/Tech/atelier.php
@@ -1,37 +1,25 @@
 <div class="container-fluid">
-  <div class="row">
-  			<div class="col-md-10">
-       <svg id="TopSVG" viewBox="0 0 1200 600" class="bg-dark border border-warning"></svg>
+
+ <div class="row m-2">
+   <h3><i class="fas fa-image text-primary"></i> Edition graphique du synoptique <strong id="idAtelierTitle"></strong></h3>
+
+   <div class ="ml-auto btn-group align-items-start">
+        <button type="button" onclick="Atelier_Sauvegarder()" class="btn btn-outline-success"><i class="fas fa-save"></i> Sauvegarder</button>
+        <button type="button" onclick="Go_to_dls_status()" class="btn btn-info"><i class="fas fa-eye"></i> Statut Global</button>
+         <!-- <button type="button" class="btn btn-sm btn-primary rounded-circle"><i class="fas fa-plus"></i></button>-->
+   </div>
+ </div>
+
+
+  			<div id="idTopDIV" class="row">
+       <svg id="TopSVG" width="100%" viewBox="0 0 1200 600"
+            style="background-color: #666"
+            xmlns="http://www.w3.org/2000/svg" xmlns:html="http://www.w3.org/1999/xhtml"></svg>
 					</div>
-<!--
-  			<div class="col-md-12">
-    			<div class="col-md-2">
-         <select id="select-classes" onchange="Charger_icone()"></select>
-       </div>
-    			<div class="col-md-3">
-         <select id="select-icones"></select>
-       </div>
-    			<div class="col-md-1">
-         <button class="btn btn-success" onclick="Ajouter_icone()">Ajouter l'icone</button>
-       </div>
-    			<div class="col-md-1">
-         <button class="btn btn-success" onclick="Ajouter_lien()">Ajouter un lien</button>
-       </div>
-    			<div class="col-md-1">
-         <button class="btn btn-success" onclick="Ajouter_rectangle()">Ajouter un rectangle</button>
-       </div>
-     </div>
 
--->
-  			<div class="col-md-2 border border-info">
-       <div class="btn-group btn-block m-1" role="group">
-         <button class="btn btn-success" onclick="Sauvegarder_synoptique()"><i class="fas fa-save"> </i></button>
-         <button class="btn btn-outline-danger ml-1" onclick="Supprimer_svg()"><i class="fas fa-trash"> </i></button>
-       </div>
-    			<div class="" id="WTD-ctrl-panel"><p>Rien n'est sélectionné</p></div>
-     </div>
-  </div>
 
-<script src="/js/atelier.js" type="text/javascript"></script>
 <!-- Container -->
 </div>
+
+<script src="/js/graphique.js" type="text/javascript"></script>
+<script src="/js/tech/atelier.js" type="text/javascript"></script>
