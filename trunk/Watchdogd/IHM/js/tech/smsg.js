5c6d50f8e5ba39626a594098e43fa82ec5c8315f
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2021-02-01 20:19:14 +0000

    IHM: Revue positionnement SMS
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@5309 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/IHM/js/tech/smsg.js
+++ trunk/Watchdogd/IHM/js/tech/smsg.js
@@ -1,12 +1,9 @@
  document.addEventListener('DOMContentLoaded', Load_page, false);
 
- function SMS_refresh ( )
-  { Load_config_sms();
-  }
 /************************************ Envoi les infos de modifications synoptique *********************************************/
  function SMS_Sauver_parametre ( )
   { var json_request = JSON.stringify(
-     { instance:      $('#idTargetInstance2').val(),
+     { instance:      $('#idTargetInstance').val(),
        tech_id:       $('#idGSMTechID').val(),
        ovh_service_name: $('#idGSMOVHServiceName').val(),
        ovh_application_key: $('#idGSMOVHApplicationKey').val(),
@@ -19,14 +16,14 @@
 /************************************ Demande l'envoi d'un SMS de test ********************************************************/
  function SMS_test ( target )
   { var json_request = JSON.stringify(
-     { instance: $('#idTargetInstance2').val(),
+     { instance: $('#idTargetInstance').val(),
        mode: target
      });
     Send_to_API ( 'PUT', "/api/process/smsg/send", json_request, null );
   }
 /********************************************* Appelé au chargement de la page ************************************************/
- function Load_config_sms ()
-  { Send_to_API ( "GET", "/api/process/smsg/status?instance="+$('#idTargetInstance2').val(), null, function(Response)
+ function SMS_Load_config ()
+  { Send_to_API ( "GET", "/api/process/smsg/status?instance="+$('#idTargetInstance').val(), null, function(Response)
      { if (Response.thread_is_running) { $('#idAlertThreadNotRunning').hide(); }
                                   else { $('#idAlertThreadNotRunning').show(); }
        $('#idGSMTechID').val( Response.tech_id );
@@ -41,11 +38,11 @@
   }
 /********************************************* Appelé au chargement de la page ************************************************/
  function Load_page ()
-  { $('#idTargetInstance2').empty();
+  { $('#idTargetInstance').empty();
     Send_to_API ( "GET", "/api/process/smsg/list", null, function(Response)
      { $.each ( Response.gsms, function ( i, gsm )
-        { $('#idTargetInstance2').append("<option value='"+gsm.instance+"'>"+
+        { $('#idTargetInstance').append("<option value='"+gsm.instance+"'>"+
                                           gsm.tech_id+ " sur " +gsm.instance+"</option>"); } );
-       Load_config_sms ();
+       SMS_Load_config ();
      }, null );
   }
