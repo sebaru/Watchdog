b2c23b6fc14b361a692677118b08acd460c08b3f
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2021-02-02 14:46:05 +0000

    IHM: Correction coquille nom d'event
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@5319 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/IHM/Tech/msrv.php
+++ trunk/Watchdogd/IHM/Tech/msrv.php
@@ -9,7 +9,7 @@
        <div class="col form-group">
 					     <div class="input-group">
 						     <label class="col-5 col-sm-4 col-form-label text-right">Choix de l'instance</label>
-           <select id="idTargetInstance" class="custom-select border-info" onchanged="MSRV_Load_config()"></select>
+           <select id="idTargetInstance" class="custom-select border-info" onchange="MSRV_Load_config()"></select>
      					</div>
   					</div>
 
