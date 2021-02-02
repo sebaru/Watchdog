bff98e589e2fa450cacc166cf6dfba13e4bf0630
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2021-02-02 07:17:31 +0000

    IHM: Passage en chart js 2.9.4
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@5311 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/IHM/Home/footer.php
+++ trunk/Watchdogd/IHM/Home/footer.php
@@ -17,7 +17,7 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.min.js" type="text/javascript"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.21/fh-3.1.7/r-2.2.5/datatables.min.js"></script>
   </body>
 </html>
