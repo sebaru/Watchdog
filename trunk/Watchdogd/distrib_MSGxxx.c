72de0c86d783e1cb08633ca1870ccceb8727d2ac
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2021-02-13 20:33:55 +0000

    MSRV: Préparation des messages dynamiques
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@5349 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/distrib_MSGxxx.c
+++ trunk/Watchdogd/distrib_MSGxxx.c
@@ -38,7 +38,8 @@
 /* Entrée/Sortie: rien                                                                                                        */
 /******************************************************************************************************************************/
  static void Gerer_arrive_MSG_event_dls_on ( struct DLS_MESSAGES *msg )
-  { gchar chaine[512], *date_create;
+  { gchar libelle[128], chaine[512], *date_create;
+    gchar prefixe[128], tech_id[32], acronyme[64], suffixe[128];
     struct CMD_TYPE_MESSAGE *message;
     struct CMD_TYPE_HISTO histo;
     struct timeval tv;
@@ -49,6 +50,49 @@
     memset ( &histo, 0, sizeof(struct CMD_TYPE_HISTO) );
     memcpy( &histo.msg, message, sizeof(struct CMD_TYPE_MESSAGE) );                                       /* Ajout dans la DB */
 
+    g_snprintf ( libelle, sizeof(libelle), "%s", message->libelle );
+    memset ( suffixe, 0, sizeof(suffixe) );
+/************************************* Conversation du lessage dynalique ******************************************************/
+    while ( sscanf ( libelle, "%128[^$]$%32[^:]:%64[a-zA-Z0-9_]%128c", prefixe, tech_id, acronyme, suffixe ) == 4 )
+     { gchar result[128];
+       gpointer dls_data_p = NULL;
+       g_snprintf( result, sizeof(result), "%s", prefixe );                                                       /* Prologue */
+       gint type = Rechercher_DICO_type ( tech_id, acronyme );
+       if (type == MNEMO_ENTREE_ANA) 
+        { Dls_data_get_AI ( tech_id, acronyme, &dls_data_p );
+          struct DLS_AI *ai = dls_data_p;
+          if (ai)
+           { /*if (ai->val_ech-roundf(ai->val_ech) == 0.0)
+              { g_snprintf( chaine, sizeof(chaine), "%.0f %s", ai->val_ech, ai->unite ); }
+             else*/
+              { g_snprintf( chaine, sizeof(chaine), "%.2f %s", ai->val_ech, ai->unite ); }
+           }
+          else g_snprintf( chaine, sizeof(chaine), "erreur" );
+          g_strlcat ( result, chaine, sizeof(result) );
+        }
+       g_strlcat ( result, suffixe, sizeof(result) );
+       g_snprintf( libelle, sizeof(libelle), "%s", result );
+       memset ( suffixe, 0, sizeof(suffixe) );
+/*               g_snprintf( chaine, sizeof(chaine), "%d %s", ci->valeur, ci->unite ); /* Row1 = unite */
+/*               g_snprintf( chaine, sizeof(chaine), "%d heure et %d minute", tm.tm_hour, tm.tm_min );*/
+/*            break;
+       case MNEMO_REGISTRE:
+             { Dls_data_get_R ( tech_id, acronyme, dlsdata_p );
+               struct DLS_REGISTRE *reg = *dlsdata_p;
+               if (reg)
+                { if (reg->valeur-roundf(reg->valeur) == 0.0)
+                   { g_snprintf( chaine, sizeof(chaine), "%.0f %s", reg->valeur, reg->unite ); }
+                  else
+                   { g_snprintf( chaine, sizeof(chaine), "%.2f %s", reg->valeur, reg->unite ); }
+                }
+               else g_snprintf( chaine, sizeof(chaine), "erreur" );
+             }
+            break;
+       default: return(NULL);
+     }
+    * */
+     }
+    Info_new( Config.log, Config.log_msrv, LOG_DEBUG, "%s: Message parsé final: %s", __func__, libelle );
 /***************************************** Création de la structure interne de stockage ***************************************/
     histo.alive = TRUE;
     gettimeofday( &tv, NULL );
