e0cf18ab98ae0b88293547023f2cf07695cf51d2
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2021-07-11 18:57:02 +0000

    IHM: Ajout de syn_vmc + uniformisation couleurs chambres
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@5844 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/IHM/js/tech/synoptiques.js
+++ trunk/Watchdogd/IHM/js/tech/synoptiques.js
@@ -123,7 +123,8 @@
                "syn_communication.png", "syn_confort.png", "syn_cuisine.png", "syn_energie.png", "syn_electricite.png",
                "syn_garage.png", "syn_jardin.png", "syn_piscine.png", "syn_luminaires.png", "syn_panneau_solaire.png",
                "syn_salle_de_bain.png", "syn_jeux.png", "syn_salon.png", "syn_volets.png", "syn_spot.png",
-               "syn_maintenance.png", "syn_parametres.png", "syn_horloge.png", "syn_reseau.png", "syn_sonorisation.png" ];
+               "syn_maintenance.png", "syn_parametres.png", "syn_horloge.png", "syn_reseau.png", "syn_sonorisation.png",
+               "syn_vmc.png" ];
 
     liste = $("#idSynEditImageListe");
     liste.empty();
