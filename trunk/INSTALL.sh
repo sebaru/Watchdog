c26c589ca00b51a92061aaf33ab16cf8359daafc
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2021-08-29 15:07:33 +0000

    FCT: INSTALL: Donne le choix entre mode Système et mode User
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@5950 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/INSTALL.sh
+++ trunk/INSTALL.sh
@@ -3,6 +3,13 @@
 SOCLE=`grep "^ID=" /etc/os-release | cut -f 2 -d '='`
 USER=`whoami`
 
+read -p "Install for (S)ystemMode or (U)serMode (s/u) ?" -n1 USERMODE
+if ( [ "$USERMODE" = "u" ] )
+ then USERMODE = "Y"; echo "Installing in user mode";
+ else USERMODE = "N"; echo "Installing in system mode";
+fi
+
+sleep 4
 
 if [ "$SOCLE" = "fedora" ]
  then
@@ -10,12 +17,13 @@ if [ "$SOCLE" = "fedora" ]
   dnf update -y
   dnf install -y subversion libtool automake autoconf gcc gcc-c++ redhat-rpm-config openssl
   dnf install -y glib2-devel bison flex readline-devel giflib-devel
-  dnf install -y libcurl-devel nut-devel mariadb-devel zeromq-devel libuuid-devel
+  dnf install -y nut-devel mariadb-devel zeromq-devel libuuid-devel
   dnf install -y gtk3-devel goocanvas2-devel popt-devel libsoup-devel
-  dnf install -y gtksourceview2-devel goocanvas-devel json-glib-devel gammu-devel
+  dnf install -y json-glib-devel gammu-devel
   dnf install -y mpg123 sox mosquitto-devel libusb-devel
-  dnf install -y libgnomeui-devel librsvg2-devel
+  dnf install -y librsvg2-devel
   dnf install -y git
+
   git clone https://github.com/strophe/libstrophe.git
   cd libstrophe
   ./bootstrap.sh
@@ -24,6 +32,7 @@ if [ "$SOCLE" = "fedora" ]
   make install
   cd ..
   rm -rf libstrophe
+
   wget https://www.phidgets.com/downloads/phidget22/libraries/linux/libphidget22.tar.gz
   tar xzf libphidget22.tar.gz
   cd libphidget22-*
@@ -48,8 +57,8 @@ if [ "$SOCLE" = "debian" ] || [ "$SOCLE" = "raspbian" ]
 
   apt install -y subversion libtool automake autoconf gcc git cmake openssl
   apt install -y libglib2.0-dev bison flex libreadline-dev libgif-dev
-  apt install -y libupsclient-dev libcurl4-gnutls-dev libssl-dev default-libmysqlclient-dev libstrophe-dev libgammu-dev
-  apt install -y liblircclient-dev libpopt-dev libssl-dev libmariadbclient-dev libzmq3-dev
+  apt install -y libupsclient-dev libssl-dev default-libmysqlclient-dev libstrophe-dev libgammu-dev
+  apt install -y libpopt-dev libssl-dev libmariadbclient-dev libzmq3-dev
   apt install -y sox libsox-fmt-all python3-pip mpg123
   apt install -y libjson-glib-dev libmosquitto-dev
   apt install -y libgtk-3-dev libgoocanvas-2.0-dev
@@ -58,18 +67,21 @@ if [ "$SOCLE" = "debian" ] || [ "$SOCLE" = "raspbian" ]
   apt install -y libphidget22 libphidget22-dev
 fi
 
-    if [ "$SOCLE" = "fedora" ]
+  if [ "$USERMODE" = "N" ]
+   then
+    if [ "$SOCLE" = "fedora" -a "$headless" = "N" ]
      then dnf -y install mariadb-server
     fi
     if [ "$SOCLE" = "debian" ]
      then apt -y install mariadb-server
-	   mysql_install_db
+          mysql_install_db
     fi
     systemctl restart mariadb
 
     NEWPASSWORD=`openssl rand -base64 32`
     /usr/bin/mysqladmin -u root create WatchdogDB
     echo "CREATE USER 'watchdog' IDENTIFIED BY '$NEWPASSWORD'; GRANT ALL PRIVILEGES ON WatchdogDB.* TO watchdog; FLUSH PRIVILEGES; " | mysql -u root WatchdogDB
+  fi
 
 svn co https://svn.abls-habitat.fr/repo/Watchdog/prod watchdogabls
 cd watchdogabls
@@ -80,8 +92,15 @@ cd ..
 rm -rf watchdogabls
 systemctl daemon-reload
 
-    echo "La base de données 'WatchdogDB' a été crée, ainsi que l'utilisateur 'watchdog'."
-    echo "Son mot de passe est "$NEWPASSWORD
-    echo "Le point d'accès pour poursuivre l'installation est https://"`hostname`":5560/install"
+  if [ "$USERMODE" = "N" ]
+    then
+      echo "La base de données 'WatchdogDB' a été crée, ainsi que l'utilisateur 'watchdog'."
+      echo "Son mot de passe est "$NEWPASSWORD
+      systemctl --user disable Watchdogd --now
+      systemctl enable Watchdogd --now
+    else
+      systemctl --user enable Watchdogd --now
+      systemctl disable Watchdogd --now
+  fi
 
-systemctl enable Watchdogd --now
+  echo "Le point d'accès pour poursuivre l'installation est https://"`hostname`":5560/install"
