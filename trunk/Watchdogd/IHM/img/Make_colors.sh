d90a427715f83e6b2c7eb90a8d89d128feb5428d
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2021-10-03 13:51:42 +0000

    FCT: IHM: Ajout nice et ionice sur fabrication des icones
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@6024 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/IHM/img/Make_colors.sh
+++ trunk/Watchdogd/IHM/img/Make_colors.sh
@@ -4,15 +4,15 @@
         do
             FILENAME=$(basename $SRC "_source.svg")
             echo Parsing $SRC into ${FILENAME}_colors.svg
-            sed s/c8c8c8/00ff00/g $SRC > ${FILENAME}_green.svg
-            sed s/c8c8c8/ff0000/g $SRC > ${FILENAME}_red.svg
-            sed s/c8c8c8/0000ff/g $SRC > ${FILENAME}_blue.svg
-            sed s/c8c8c8/ffff00/g $SRC > ${FILENAME}_yellow.svg
-            sed s/c8c8c8/ffbe00/g $SRC > ${FILENAME}_orange.svg
-            sed s/c8c8c8/006400/g $SRC > ${FILENAME}_darkgreen.svg
-            sed s/c8c8c8/000000/g $SRC > ${FILENAME}_black.svg
-            sed s/c8c8c8/ffffff/g $SRC > ${FILENAME}_white.svg
-            sed s/c8c8c8/7b7b7b/g $SRC > ${FILENAME}_gray.svg
-            sed s/c8c8c8/add8e6/g $SRC > ${FILENAME}_lightblue.svg
-	    svn add ${FILENAME}_*.svg
-    done
+            nice -10 sed s/c8c8c8/00ff00/g $SRC > ${FILENAME}_green.svg
+            nice -10 sed s/c8c8c8/ff0000/g $SRC > ${FILENAME}_red.svg
+            nice -10 sed s/c8c8c8/0000ff/g $SRC > ${FILENAME}_blue.svg
+            nice -10 sed s/c8c8c8/ffff00/g $SRC > ${FILENAME}_yellow.svg
+            nice -10 sed s/c8c8c8/ffbe00/g $SRC > ${FILENAME}_orange.svg
+            nice -10 sed s/c8c8c8/006400/g $SRC > ${FILENAME}_darkgreen.svg
+            nice -10 sed s/c8c8c8/000000/g $SRC > ${FILENAME}_black.svg
+            nice -10 sed s/c8c8c8/ffffff/g $SRC > ${FILENAME}_white.svg
+            nice -10 sed s/c8c8c8/7b7b7b/g $SRC > ${FILENAME}_gray.svg
+            nice -10 sed s/c8c8c8/add8e6/g $SRC > ${FILENAME}_lightblue.svg
+	           nice -10 svn add ${FILENAME}_*.svg
+        done
