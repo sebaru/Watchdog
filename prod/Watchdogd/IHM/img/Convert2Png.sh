04a914a9f029bd235113f97212807edac32bfca0
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2021-10-10 18:32:09 +0000

    Merging changes from PROD Rev 6050 to TRUNK Rev 6051 ->
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@6052 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- prod/Watchdogd/IHM/img/Convert2Png.sh
+++ prod/Watchdogd/IHM/img/Convert2Png.sh
@@ -3,5 +3,11 @@ for SRC in $( ls *.svg );
         do
             FILENAME=$(basename $SRC ".svg")
             echo Parsing ${SRC} into ${FILENAME}.png
-            nice -10 ionice -c 3 sh -c 'inkscape '${SRC}' -o '${FILENAME}'.png; svn add '${FILENAME}'.png' &
-	done
+            nice -10 ionice -c 3 sh -c 'inkscape '${SRC}' -o '${FILENAME}'.png;' &
+	       done
+
+for job in `jobs -p`
+        do
+           wait $job
+        done
+svn add *.png &>/dev/null
