1b81f2d7dac0bc1f63843510a14d36fc62d01d67
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2021-10-12 19:29:46 +0000

    Merging changes from PROD Rev 6056 to TRUNK Rev 6059 ->
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@6060 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- prod/Watchdogd/IHM/js/tech/synoptiques.js
+++ prod/Watchdogd/IHM/js/tech/synoptiques.js
@@ -125,8 +125,9 @@
     images = [ "syn_maison.png", "syn_communication.png", "syn_reseau.png",
                "syn_buanderie.png", "syn_camera.png", "syn_chambre_double.png", "syn_chambre_simple.png",
                "syn_cuisine.png", "syn_garage.png", "syn_jardin.png", "syn_piscine.png",
-               "syn_salle_de_bain.png", "syn_salon.png", "syn_jeux.png",
-               "syn_volets.png", "syn_luminaires.png", "syn_spot.png", "syn_sonorisation.png",
+               "syn_salle_de_bain.png", "syn_salon.png", "syn_jeux.png", "syn_tele.png",
+               "syn_volets.png", "syn_luminaires.png",
+               "syn_spot.png", "syn_sonorisation.png",
                "syn_maintenance.png", "syn_parametres.png", "syn_horloge.png",
                "syn_confort.png", "syn_vmc.png",
                "syn_energie.png", "syn_electricite.png","syn_ups.png", "syn_panneau_solaire.png" ];
