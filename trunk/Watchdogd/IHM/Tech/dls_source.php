8f912367c7b5fec26e9151f194212baf2685c624
sebastien <sebastien@9f22af27-4b39-4cfe-9f85-4bed9fef4693>
2021-11-11 08:42:05 +0000

    IHM: Correction affichage
    git-svn-id: https://svn.abls-habitat.fr/repo/Watchdog@6152 9f22af27-4b39-4cfe-9f85-4bed9fef4693

--- trunk/Watchdogd/IHM/Tech/dls_source.php
+++ trunk/Watchdogd/IHM/Tech/dls_source.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
 
  <div class="row m-2">
-   <h3><i class="fas fa-code text-primary"></i> Edition D.L.S '<strong id="idSourceTitle"></strong>'</h3>
+   <h3><i class="fas fa-code text-primary"></i> Edition D.L.S <strong id="idSourceTitle"></strong></h3>
 
    <div class ="ml-auto btn-group align-items-start">
         <button type="button" onclick="Compiler()" class="btn btn-outline-success"><i class="fas fa-coffee"></i> Compiler</button>
