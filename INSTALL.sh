bf35d90000d42b64615a4e6b44a0d6e2aee48066
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2021-11-30 01:17:19 +0100

    PROCESS: Add OpenSSL dependencie

--- INSTALL.sh
+++ INSTALL.sh
@@ -25,7 +25,7 @@ if [ "$SOCLE" = "fedora" ]
   echo "Installing Fedora dependencies"
   dnf update -y
   dnf install -y subversion libtool automake autoconf gcc gcc-c++ redhat-rpm-config
-  dnf install -y glib2-devel bison flex giflib-devel
+  dnf install -y glib2-devel bison flex giflib-devel openssl
   dnf install -y nut-devel mariadb-devel zeromq-devel libuuid-devel
   dnf install -y gtk3-devel goocanvas2-devel popt-devel libsoup-devel
   dnf install -y json-glib-devel gammu-devel
@@ -67,7 +67,7 @@ if [ "$SOCLE" = "debian" ] || [ "$SOCLE" = "raspbian" ]
     apt install -y gcc-8-base
   fi
 
-  apt install -y subversion libtool automake autoconf gcc git cmake
+  apt install -y subversion libtool automake autoconf gcc git cmake openssl
   apt install -y libglib2.0-dev bison flex libgif-dev
   apt install -y libupsclient-dev libssl-dev default-libmysqlclient-dev libstrophe-dev libgammu-dev
   apt install -y libpopt-dev libssl-dev libmariadbclient-dev libzmq3-dev
