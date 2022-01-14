4a79f60183524d760413524645258407de2f447d
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2022-01-14 13:37:49 +0100

    IHM: Delete Modbus MAP button

--- Watchdogd/IHM/Tech/io_config.php
+++ Watchdogd/IHM/Tech/io_config.php
@@ -12,10 +12,9 @@
  <div class="col p-1">
   <div class="card h-100 text-center bg-light">
    <div class="card-header">Les I/O Wago</div>
-   <a href="/tech/modbus_map"><img src="/img/wago_750342.webp" class="card-img-top wtd-img-connecteur" alt="Wago 750-342"></a>
+   <a href="/tech/modbus"><img src="/img/wago_750342.webp" class="card-img-top wtd-img-connecteur" alt="Wago 750-342"></a>
    <div class="card-body">
         <button type="button" onclick="Redirect('/tech/modbus')" class="btn btn-block m-1 btn-primary btn-sm"><i class="fas fa-list"></i> Configurer</button>
-        <button type="button" onclick="Redirect('/tech/modbus_map')" class="btn btn-block m-1 btn-primary btn-sm"><i class="fas fa-directions"></i> Mapper</button>
    </div>
   </div>
  </div>
@@ -46,16 +45,6 @@
    <a href="/tech/audio"><img src="/img/audio.png" class="card-img-top wtd-img-connecteur" alt="Diffusion Audio"></a>
    <div class="card-body">
         <button type="button" onclick="Redirect('/tech/audio')" class="btn btn-block m-1 btn-primary btn-sm"><i class="fas fa-list"></i> Configurer</button>
-   </div>
-  </div>
- </div>
-
- <div class="col p-1">
-  <div class="card h-100 text-center bg-light">
-   <div class="card-header">Les I/O Vocales</div>
-   <a href="/tech/"><img src="/img/commande_vocale.jpg" class="card-img-top wtd-img-connecteur" alt="Commandes Vocales"></a>
-   <div class="card-body">
-        <button type="button" onclick="Redirect('/tech/command_vocale')" class="btn btn-block m-1 btn-primary btn-sm"><i class="fas fa-directions"></i> Mapper</button>
    </div>
   </div>
  </div>
@@ -106,7 +95,6 @@
    <a href="/tech/meteo"><img src="/img/meteo.svg" class="card-img-top wtd-img-connecteur" alt="La météo"></a>
    <div class="card-body">
         <button type="button" onclick="Redirect('/tech/meteo')" class="btn btn-block m-1 btn-primary btn-sm"><i class="fas fa-list"></i> Configurer</button>
-        <button type="button" disabled class="btn btn-block m-1 btn-primary btn-sm"><i class="fas fa-directions"></i> Mapper</button>
    </div>
   </div>
  </div>
@@ -125,7 +113,7 @@
 </div>
 
 <hr>
-<h4><i class="fas fa-directions text-primary"></i> Mappings</h4>
+<h4><i class="fas fa-directions text-primary"></i> Mappings Textes et Vocaux</h4>
 <div class="row justify-content-center row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-4">
 
  <div class="col p-1">
@@ -134,6 +122,16 @@
    <a href="/tech/command_text"><img src="/img/commande_texte.png" class="card-img-top wtd-img-connecteur" alt="Commandes Text"></a>
    <div class="card-body">
         <button type="button" onclick="Redirect('/tech/command_text')" class="btn btn-block m-1 btn-primary btn-sm"><i class="fas fa-directions"></i> Mapper</button>
+   </div>
+  </div>
+ </div>
+
+ <div class="col p-1">
+  <div class="card h-100 text-center bg-light">
+   <div class="card-header">Les I/O Vocales</div>
+   <a href="/tech/command_voc"><img src="/img/commande_vocale.jpg" class="card-img-top wtd-img-connecteur" alt="Commandes Vocales"></a>
+   <div class="card-body">
+        <button type="button" onclick="Redirect('/tech/command_vocale')" class="btn btn-block m-1 btn-primary btn-sm"><i class="fas fa-directions"></i> Mapper</button>
    </div>
   </div>
  </div>
