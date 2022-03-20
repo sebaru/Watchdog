9e9cb947111bc70b6e0be03e07079b8d935af0e3
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2022-03-20 18:27:45 +0100

    PROCESS: add websocket connect target

--- Watchdogd/process.c
+++ Watchdogd/process.c
@@ -213,9 +213,11 @@
      }
 
     module->zmq_from_bus  = Zmq_Connect ( ZMQ_SUB, "listen-to-bus", "inproc", ZMQUEUE_LOCAL_BUS, 0 );
+
     module->Master_session = soup_session_new();
     g_object_set ( G_OBJECT(module->Master_session), "ssl-strict", FALSE, NULL );
     static gchar *protocols[] = { "live-slaves", NULL };
+    g_snprintf(chaine, sizeof(chaine), "wss://%s:5559/bus", Json_get_string ( Config.config, "master_hostname" ) );
     soup_session_websocket_connect_async ( module->Master_session, soup_message_new ( "GET", chaine ),
                                             NULL, protocols, g_cancellable_new(), SubProcess_ws_on_master_connected, module );
 
