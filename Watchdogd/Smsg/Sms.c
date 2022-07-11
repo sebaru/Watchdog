788f9cef21d85a19c256d4983e813540a1d62d8c
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2022-07-11 20:46:25 +0200

    SMSG: Use /mapping/search_txt to find mapping

--- Watchdogd/Smsg/Sms.c
+++ Watchdogd/Smsg/Sms.c
@@ -341,7 +341,7 @@
      }
     g_list_free(recipients);
     Json_node_unref ( UsersNode );
-    Http_Post_to_local_BUS_AI ( module, vars->ai_nbr_sms, ++vars->nbr_sms, TRUE );
+    Http_Post_to_local_BUS_AI ( module, vars->ai_nbr_sms, vars->nbr_sms, TRUE );
   }
 /******************************************************************************************************************************/
 /* Envoyer_sms: Envoi un sms                                                                                                  */
@@ -428,7 +428,7 @@
     Json_node_add_string ( RootNode, "thread_tech_id", "_COMMAND_TEXT" );
     Json_node_add_string ( RootNode, "thread_acronyme", texte );
 
-    JsonNode *MapNode = Http_Post_to_global_API ( "/map/from_thread", RootNode );
+    JsonNode *MapNode = Http_Post_to_global_API ( "/mapping/search_txt", RootNode );
     Json_node_unref ( RootNode );
     if (!MapNode || Json_get_int ( MapNode, "api_status" ) != 200)
      { Info_new( Config.log, module->Thread_debug, LOG_ERR, "%s: %s: Could not get USER from API for '%s'", __func__, thread_tech_id, from );
