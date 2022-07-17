96b067049dc38235b98738a810e148a43a40e107
Sébastien L <sebastien.lefevre@abls-habitat.fr>
2022-07-17 10:06:44 +0200

    INSTALL: Fix Debian dependencies

--- INSTALL.sh
+++ INSTALL.sh
@@ -55,10 +55,10 @@ if [ "$SOCLE" = "debian" ] || [ "$SOCLE" = "raspbian" ]
     apt install -y gcc-8-base
   fi
 
-  apt install -y subversion libtool automake autoconf gcc git cmake openssl
+  apt install -y libtool automake autoconf gcc g++ git cmake openssl curl
   apt install -y libglib2.0-dev bison flex libgif-dev
   apt install -y libupsclient-dev libssl-dev default-libmysqlclient-dev libstrophe-dev libgammu-dev
-  apt install -y libpopt-dev libssl-dev libmariadb-dev
+  apt install -y libpopt-dev libssl-dev libmariadb-dev libjwt-dev
   apt install -y sox libsox-fmt-all python3-pip mpg123
   apt install -y libjson-glib-dev libgpiod-dev
   apt install -y libgtk-3-dev libgoocanvas-2.0-dev
