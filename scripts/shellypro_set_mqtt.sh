55cc86ec65ab9b554cc1299a4f292b76af5a454f
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2024-03-08 21:15:47 +0100

    SHELLY: Fix typo

--- scripts/shellypro_set_mqtt.sh
+++ scripts/shellypro_set_mqtt.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 echo Shelly $1: Setting MQTT server to $2
-curl -X POST -d '{"id":1, "method":"Mqtt.SetConfig", "params":{"config":{"enable":true, "server":"$2:1883"}}}' http://$1/rpc
+curl -X POST -d '{"id":1, "method":"Mqtt.SetConfig", "params":{"config":{"enable":true, "server":"'$2':1883"}}}' http://$1/rpc
 echo
 echo Shelly $1: Rebooting
 curl -X POST -d '{"id":1, "method":"Shelly.Reboot"}' http://$1/rpc
