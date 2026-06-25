2fb9d4a06b3ac6800b2887ddd4da4090dcc957c6
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2026-06-25 14:21:25 +0200

    REFINE: Add sudo to package installation commands in install_deps.sh for proper permissions

--- install_deps.sh
+++ install_deps.sh
@@ -11,12 +11,12 @@ fi
 if [ "$SOCLE" = "fedora" ]
  then
   echo "Installing Fedora dependencies"
-  dnf install -y git libtool cmake gcc pkg-config
-  dnf install -y glib2-devel openssl-devel json-glib-devel libuuid-devel
-  dnf install -y systemd-devel libjwt-devel mosquitto-devel
-  dnf install -y popt-devel libcurl-devel
-  dnf install -y gammu-devel libgpiod-devel nut-devel libstrophe-devel
-  dnf install -y libphidget22 libphidget22-devel
+  sudo dnf install -y git libtool cmake gcc pkg-config
+  sudo dnf install -y glib2-devel openssl-devel json-glib-devel libuuid-devel
+  sudo dnf install -y systemd-devel libjwt-devel mosquitto-devel
+  sudo dnf install -y popt-devel libcurl-devel
+  sudo dnf install -y gammu-devel libgpiod-devel nut-devel libstrophe-devel
+  sudo dnf install -y libphidget22 libphidget22-devel
 
 fi
 
@@ -24,25 +24,25 @@ if [ "$SOCLE" = "debian" ] || [ "$SOCLE" = "raspbian" ] || [ "$SOCLE" = "ubuntu"
  then
   echo "Installing debian/ubuntu dependencies"
 
-  apt update -y
+  sudo apt update -y
 
   if [ "$SOCLE" = "raspbian" ]
    then
-    apt install -y gcc-8-base
+    sudo apt install -y gcc-8-base
   fi
 
   if [ "$SOCLE" = "ubuntu" ]
    then
-    apt install -y software-properties-common
-    add-apt-repository universe -y
-    apt update -y
+    sudo apt install -y software-properties-common
+    sudo add-apt-repository universe -y
+    sudo apt update -y
   fi
 
-  apt install -y git libtool cmake gcc pkg-config
-  apt install -y libglib2.0-dev libssl-dev libjson-glib-dev uuid-dev
-  apt install -y libsystemd-dev libjwt-dev libmosquitto-dev
-  apt install -y libpopt-dev libcurl4-openssl-dev
-  apt install -y libgammu-dev libgpiod-dev libupsclient-dev libstrophe-dev
+  sudo apt install -y git libtool cmake gcc pkg-config
+  sudo apt install -y libglib2.0-dev libssl-dev libjson-glib-dev uuid-dev
+  sudo apt install -y libsystemd-dev libjwt-dev libmosquitto-dev
+  sudo apt install -y libpopt-dev libcurl4-openssl-dev
+  sudo apt install -y libgammu-dev libgpiod-dev libupsclient-dev libstrophe-dev
   curl -fsSL https://www.phidgets.com/downloads/setup_linux | bash -
-  apt install -y libphidget22 libphidget22-dev
+  sudo apt install -y libphidget22 libphidget22-dev
 fi
