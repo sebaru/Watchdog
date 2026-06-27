4af5a42eee39e2d34a46848044f174a4b7fdbb5d
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2026-06-27 20:42:52 +0200

    REFINE: Remove sudo from package installation commands in install_deps.sh for consistency and improved permissions handling

--- install_deps.sh
+++ install_deps.sh
@@ -8,15 +8,20 @@ if [ "$(whoami)" != "root" ]
    exit 1
 fi
 
+groupadd abls
+
 if [ "$SOCLE" = "fedora" ]
  then
-  echo "Installing Fedora dependencies"
-  sudo dnf install -y git libtool cmake gcc pkg-config
-  sudo dnf install -y glib2-devel openssl-devel json-glib-devel libuuid-devel
-  sudo dnf install -y systemd-devel libjwt-devel mosquitto-devel
-  sudo dnf install -y popt-devel libcurl-devel
-  sudo dnf install -y gammu-devel libgpiod-devel nut-devel libstrophe-devel
-  sudo dnf install -y libphidget22 libphidget22-devel
+  echo "Configuring ABLS-RPMS repository"
+  curl -fsSL https://rpms.abls-habitat.fr/abls-rpms.repo -o /etc/yum.repos.d/abls-rpms.repo
+
+  echo "Installing RPM-based dependencies"
+  dnf install -y abls-libs-devel git libtool cmake gcc pkg-config
+  dnf install -y glib2-devel openssl-devel json-glib-devel libuuid-devel
+  dnf install -y systemd-devel libjwt-devel mosquitto-devel
+  dnf install -y popt-devel libcurl-devel
+  dnf install -y gammu-devel libgpiod-devel nut-devel libstrophe-devel
+  dnf install -y libphidget22 libphidget22-devel
 
 fi
 
@@ -24,25 +29,25 @@ if [ "$SOCLE" = "debian" ] || [ "$SOCLE" = "raspbian" ] || [ "$SOCLE" = "ubuntu"
  then
   echo "Installing debian/ubuntu dependencies"
 
-  sudo apt update -y
+  apt update -y
 
   if [ "$SOCLE" = "raspbian" ]
    then
-    sudo apt install -y gcc-8-base
+    apt install -y gcc-8-base
   fi
 
   if [ "$SOCLE" = "ubuntu" ]
    then
-    sudo apt install -y software-properties-common
-    sudo add-apt-repository universe -y
-    sudo apt update -y
+    apt install -y software-properties-common
+    add-apt-repository universe -y
+    apt update -y
   fi
 
-  sudo apt install -y git libtool cmake gcc pkg-config
-  sudo apt install -y libglib2.0-dev libssl-dev libjson-glib-dev uuid-dev
-  sudo apt install -y libsystemd-dev libjwt-dev libmosquitto-dev
-  sudo apt install -y libpopt-dev libcurl4-openssl-dev
-  sudo apt install -y libgammu-dev libgpiod-dev libupsclient-dev libstrophe-dev
+  apt install -y git libtool cmake gcc pkg-config
+  apt install -y libglib2.0-dev libssl-dev libjson-glib-dev uuid-dev
+  apt install -y libsystemd-dev libjwt-dev libmosquitto-dev
+  apt install -y libpopt-dev libcurl4-openssl-dev
+  apt install -y libgammu-dev libgpiod-dev libupsclient-dev libstrophe-dev
   curl -fsSL https://www.phidgets.com/downloads/setup_linux | bash -
-  sudo apt install -y libphidget22 libphidget22-dev
+  apt install -y libphidget22 libphidget22-dev
 fi
